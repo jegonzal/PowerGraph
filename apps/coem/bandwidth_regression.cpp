3d0a8fe7757e64ea9fcb1094d00942a03036ea99
Yucheng Low <yuchenglow@gmail.com>
2010-10-07 02:35:37 -0400

    parallel bandwidth regression

--- apps/coem/bandwidth_regression.cpp
+++ apps/coem/bandwidth_regression.cpp
@@ -67,10 +67,7 @@ int main(int argc,  char ** argv) {
   while(t.current_time() < duration_mins*60) {
     dc.barrier();
     double t1 = t.current_time();
-    for (size_t i = 0;i < dc.numprocs(); ++i) {
-      if (dc.procid() == i)  distributed_metrics::instance(&dc)->execute_bandwith_test();
-      dc.barrier();
-    }
+    distributed_metrics::instance(&dc)->execute_bandwith_test();
     std::cout << myprocid << ": test took " << (t.current_time()-t1) << " secs." << std::endl;
     dc.barrier();
     
