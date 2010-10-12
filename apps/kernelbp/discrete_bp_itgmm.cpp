d5c395935cc34168b65f0b3733f62fa2186ecd45
Yucheng Low <yuchenglow@gmail.com>
2010-10-11 23:20:40 -0400

    added kernel bp and discrete bp test

--- apps/kernelbp/discrete_bp_itgmm.cpp
+++ apps/kernelbp/discrete_bp_itgmm.cpp
@@ -45,11 +45,12 @@ size_t MAX_ITERATIONS;
 graphlab::atomic<size_t> proposal_count;
 graphlab::atomic<size_t> accept_count;
 
+graphlab::binary_factor global_edge_pot;
+
 // STRUCTS (Edge and Vertex data) =============================================>
 struct edge_data: public graphlab::unsupported_serialize {
   graphlab::unary_factor message;
   graphlab::unary_factor old_message;
-  graphlab::binary_factor edge_pot;
   
 }; // End of edge data
 
@@ -67,206 +68,6 @@ typedef graphlab::graph<vertex_data, edge_data> graph_type;
 typedef graphlab::types<graph_type> gl_types;
 
 
-/**
-  Generates a spherical guassian of a particular stddev
-  Returns log likelihood
-*/
-double gaussianrng(float &v,  double stdev) {
-  // box muller generator
-  double U0 = graphlab::random::rand01() + DBL_MIN;
-  double U1 = graphlab::random::rand01() + DBL_MIN;
-  double z0 = sqrt(-2 * log(U0)) * cos(2 * M_PI * U1);
-
-  v = z0;
-  return (v * v) / (2 * stdev * stdev);;
-}
-
-/**
-  Makes the largest particle have a size of 1
-*/
-void normalize_particles(std::vector<particle> &particles) {
-  float totalweight = 0;
-  for (size_t i = 0;i < particles.size(); ++i) {
-    totalweight += particles[i].weight;
-  }
-  for (size_t i = 0;i < particles.size(); ++i) {
-    particles[i].weight /= totalweight;
-    // set a minimum threshold
-    if (particles[i].weight < 1E-12) particles[i].weight = 1E-12;
-  }
-}
-
-/**
-  Computes the message along the edge.
-  The message requires the shape of the target vertex
-*/
-void sample_message(gl_types::iscope& scope,
-                    graphlab::edge_id_t edge) {
-  // source and dest vertices
-  const vertex_data &srcvdata = scope.const_vertex_data();
-  const vertex_data &destvdata = scope.const_neighbor_vertex_data(scope.target(edge));
-  // in and out edges
-  edge_data &outedgedata = scope.edge_data(edge);
-  const edge_data &inedgedata = scope.const_edge_data(scope.reverse_edge(edge));
-
-  // get the shape of the particles
-  outedgedata.message = destvdata.belief;
-  // update the weights
-  for (size_t i = 0; i < outedgedata.message.size(); ++i) {
-    double oldweight = outedgedata.message[i].weight;
-    outedgedata.message[i].weight = 0;
-    for (size_t j = 0;j < inedgedata.message.size(); ++j) {
-      // outgoing potential * src belief / by incoming message
-      outedgedata.message[i].weight  +=
-                outedgedata.edgepot(srcvdata.belief[j].x, outedgedata.message[i].x) *
-                srcvdata.belief[j].weight / inedgedata.message[j].weight;
-    }
-    outedgedata.message[i].weight = outedgedata.message[i].weight * damping
-                                    + oldweight * (1.0 - damping);
-  }
- normalize_particles(outedgedata.message);
-}
-
-
-/**
-Updates the belief on the current vertex.
-Requires that the current vertex has the same particles
-as all the incoming messages
-*/
-void update_belief(gl_types::iscope& scope) {
-  // source and dest vertices
-  vertex_data &vdata = scope.vertex_data();
-  for (size_t i = 0;i < vdata.belief.size(); ++i) {
-    vdata.belief[i].weight = vdata.vertexpot(vdata.belief[i].x);
-  }
-  normalize_particles(vdata.belief);
-  foreach(graphlab::edge_id_t ineid, scope.in_edge_ids()) {
-    const edge_data &inedgedata = scope.const_edge_data(ineid);
-    for (size_t i = 0;i < vdata.belief.size(); ++i) {
-      vdata.belief[i].weight *= inedgedata.message[i].weight;
-    }
-    normalize_particles(vdata.belief);
-  }
-}
-
-
-
-/**
-Estimates  log P(v) by reevaluating all the incoming messages
-*/
-double second_order_belief_ll(gl_types::iscope& scope, float v) {
-  graphlab::edge_list out_edges = scope.out_edge_ids();
-  double ll = 0;
-  foreach(graphlab::edge_id_t outeid, out_edges) {
-    const edge_data &outedgedata = scope.const_edge_data(outeid);
-    const vertex_data &othervdata = scope.const_neighbor_vertex_data(scope.target(outeid));
-    double w = 0;
-    for (size_t j = 0;j < othervdata.belief.size(); ++j) {
-      w += outedgedata.edgepot(v, othervdata.belief[j].x) *
-                        othervdata.belief[j].weight /
-                        outedgedata.message[j].weight;
-    }
-    ll += log(w);
-  }
-  ll += (scope.vertex_data().log_vertexpot(v));
-  return ll;
-}
-
-
-double eff_particles(gl_types::iscope &scope) {
-// lets move all the particles around using MCMC
-  vertex_data &srcvdata = scope.vertex_data();
-
-  normalize_particles(srcvdata.belief);
-  // get effective number of particle weight
-  double wsqr = 0;
-  for (size_t i = 0;i < srcvdata.belief.size(); ++i) {
-    wsqr += srcvdata.belief[i].weight * srcvdata.belief[i].weight;
-  }
-  return 1.0 / wsqr;
-}
-
-/**
-  Resamples all the partciles at the current scope
-*/
-float resample_particles(gl_types::iscope& scope,
-                        double gaussian_proposal_stdev) {
-  int acceptcount = 0;
-  int proposecount = 0;
-
-  // lets move all the particles around using MCMC
-  vertex_data &srcvdata = scope.vertex_data();
-
-  // if the number of particles is fine. don't resample
-  if (eff_particles(scope) > srcvdata.belief.size() / 2 + 1) return 0;
-  update_belief(scope);
-  std::vector<float> newparticles;
-  // sample from the old belief
-  for (size_t i = 0;i < srcvdata.belief.size(); ++i) {
-    // pick a particle to use
-    double r = graphlab::random::rand01();
-    // scan the CDF
-    float cursample = srcvdata.belief[srcvdata.belief.size()-1].x;
-    double accweight = 0;
-    for (size_t j = 0;j < srcvdata.belief.size(); ++j) {
-      accweight += srcvdata.belief[j].weight;
-      if (accweight >= r) {
-        cursample = srcvdata.belief[j].x;
-        break;
-      }
-    }
-    double curll = second_order_belief_ll(scope, cursample);
-    for (size_t i = 0; i < MCMCSTEPS; ++i) {
-      float gauss = graphlab::random::gaussian_rand() * gaussian_proposal_stdev;
-      proposal_count.inc();
-      float proposal = cursample + gauss;
-      // compute forward probability
-      double proposell = second_order_belief_ll(scope, proposal);
-      // since the gaussian is symmetric...
-      double acceptll = proposell - curll;
-      proposecount++;
-      bool accept = false;
-      if (acceptll >= 0)  {
-        accept = true;
-      }
-      else {
-        // acceptreject
-        acceptll = std::exp(acceptll);
-        if (graphlab::random::rand01() <= acceptll)  accept = true;
-      }
-      if (accept) {
-        acceptcount++;
-        accept_count.inc();
-        curll = proposell;
-        cursample = proposal;
-      }
-    }
-
-    newparticles.push_back(cursample);
-  }
-
-  // fill the new belief
-  for (size_t i = 0;i < srcvdata.belief.size(); ++i) {
-    srcvdata.belief[i].x = newparticles[i];
-    srcvdata.belief[i].weight = 1.0 / srcvdata.belief.size();
-  }
-
-  
-  if(scope.vertex() == 0) {
-    std::cout << "vertex 0 particles: ";
-    for (size_t i = 0;i < srcvdata.belief.size(); ++i) {
-      std::cout  << srcvdata.belief[i].x << ":" << srcvdata.belief[i].weight << " ";
-    }
-    std::cout << "\n";
-  }
-  graphlab::edge_list in_edges = scope.in_edge_ids();
-  foreach(graphlab::edge_id_t ineid, in_edges) {
-    sample_message(scope, ineid);
-  }
-  return float(acceptcount) / proposecount;
-}
-
-
 
 // Implementations
 // ============================================================>
@@ -275,15 +76,16 @@ void bp_update(gl_types::iscope& scope,
                gl_types::ishared_data* shared_data) {
   //  std::cout << scope.vertex();;
   //  std::getchar();
-  assert(shared_data != NULL);
-  if (v.rounds >= MAX_ITERATIONS) return;
-  // Get the shared data
-  
+
   // Grab the state from the scope
   // ---------------------------------------------------------------->
   // Get the vertex data
   vertex_data& v_data = scope.vertex_data();
-  
+  if (v_data.rounds >= MAX_ITERATIONS) return;
+  if (scope.vertex() == 0) {
+    std::cout << "Vertex 0 Round " << v_data.rounds << std::endl;
+  }
+  v_data.rounds++;
   // Get the in and out edges by reference
   graphlab::edge_list in_edges = 
     scope.in_edge_ids();
@@ -317,8 +119,6 @@ void bp_update(gl_types::iscope& scope,
   // Compute outbound messages
   // ---------------------------------------------------------------->
 
-  const graphlab::binary_factor edge_factor =
-    shared_data->get_constant(EDGE_FACTOR_ID).as<graphlab::binary_factor>();
   
   
   // Send outbound messages
@@ -344,19 +144,17 @@ void bp_update(gl_types::iscope& scope,
     // temporary message
     tmp_msg.resize(out_edge.message.arity());
     tmp_msg.var() = out_edge.message.var();
-    tmp_msg.convolve(edge_factor, cavity);
+    tmp_msg.convolve(global_edge_pot, cavity);
     tmp_msg.normalize();
 
     // Damp the message
     tmp_msg.damp(out_edge.message, damping);
     
-    // Compute message residual
-    double residual = tmp_msg.residual(out_edge.old_message);
     
     // Assign the out message
     out_edge.message = tmp_msg;
   }
-  if (v.rounds < MAX_ITERATIONS) {
+  if (v_data.rounds < MAX_ITERATIONS) {
     gl_types::update_task task(scope.vertex(), bp_update);
     scheduler.add_task(task, 1.0);
   }
@@ -373,8 +171,9 @@ void construct_graph(image& img,
                     const GaussianMixture<2> &edgepot,
                     const GaussianMixture<2> &nodepot,
                      double numparticles,
-                     gl_types::graph& graph) {
-                     
+                     gl_types::graph& graph,
+                    std::vector<double> discretizationpts) {
+  size_t numasgs = discretizationpts.size();
   // initialize a bunch of particles
   for(size_t i = 0; i < img.rows(); ++i) {
     for(size_t j = 0; j < img.cols(); ++j) {
@@ -383,33 +182,29 @@ void construct_graph(image& img,
 
       // Set the node potential
       vdat.obs = img.pixel(i,j);
-      vdat.p = gmm_conditional<2>()(nodepot, 1, vdat.obs);
-      vdat.p.simplify();
-      for (size_t n = 0;n < numparticles; ++n) {
-        particle p;
-        p.x = vdat.p.sample();
-        p.weight = 1.0/numparticles;
-        vdat.belief.push_back(p);
+      vdat.potential.resize(numasgs);
+      GaussianMixture<1> np = gmm_conditional<2>()(nodepot, 01, vdat.obs);
+      for (size_t p = 0;p < numasgs; ++p) {
+        vdat.potential.logP(p) = log(np.likelihood(discretizationpts[p]));
       }
-
-      if(i == 0 && j == 0) {
-        std::cout << "vertex 0 particles: ";
-        for (size_t i = 0;i < vdat.belief.size(); ++i) {
-          std::cout  << vdat.belief[i].x << " ";
-        }
-        std::cout << "\n";
-      }
+      vdat.belief = vdat.potential;
+      vdat.belief.normalize();
       graph.add_vertex(vdat);
-
     } // end of for j in cols
   } // end of for i in rows
 
 
-
-  // Add the edges
-  
+  // make the edge potential
   edge_data edata;
-  edata.p = edgepot;
+  global_edge_pot.resize(numasgs, numasgs);
+  for (size_t p = 0; p < numasgs; ++p) {
+    for (size_t q = 0; q < numasgs; ++q) {
+      double d[2]; d[0] = discretizationpts[p]; d[1] = discretizationpts[q];
+      global_edge_pot.logP(p,q) = log(edgepot.likelihood(d));
+    }
+  }
+
+  //std::cout << edataedge_pot << std::endl;
 
   for(size_t i = 0; i < img.rows(); ++i) {
     for(size_t j = 0; j < img.cols(); ++j) {
@@ -417,18 +212,22 @@ void construct_graph(image& img,
       size_t vertid = img.vertid(i,j);
       if(i-1 < img.rows()) {
         edata.message = graph.vertex_data(img.vertid(i-1, j)).belief;
+        edata.old_message = edata.message;
         graph.add_edge(vertid, img.vertid(i-1, j), edata);
       }
       if(i+1 < img.rows()) {
         edata.message = graph.vertex_data(img.vertid(i+1, j)).belief;
+        edata.old_message = edata.message;
         graph.add_edge(vertid, img.vertid(i+1, j), edata);
       }
       if(j-1 < img.cols()) {
         edata.message = graph.vertex_data(img.vertid(i, j-1)).belief;
+        edata.old_message = edata.message;
         graph.add_edge(vertid, img.vertid(i, j-1), edata);
       }
       if(j+1 < img.cols()) {
         edata.message = graph.vertex_data(img.vertid(i, j+1)).belief;
+        edata.old_message = edata.message;
         graph.add_edge(vertid, img.vertid(i, j+1), edata);
       }
     } // end of for j in cols
@@ -468,7 +267,7 @@ int main(int argc, char** argv) {
 
   std::string gmmfile= "";
   std::string inputfile = "";
-
+  std::string logfile = "";
 
   // Parse command line arguments --------------------------------------------->
   graphlab::command_line_options clopts("Loopy BP image denoising");
@@ -493,6 +292,9 @@ int main(int argc, char** argv) {
   clopts.attach_option("mcmc",
                        &MCMCSTEPS, MCMCSTEPS,
                        "mcmc steps per resample");
+  clopts.attach_option("logfile",
+                       &logfile, std::string(""),
+                       "log file");
 
   // set default scheduler type
   clopts.scheduler_type = "splash(100)";
@@ -560,8 +362,17 @@ int main(int argc, char** argv) {
   core.set_engine_options(clopts);
 
   std::cout << "Constructing pairwise Markov Random Field. " << std::endl;
-
-  construct_graph(img, edgepot, nodepot, numparticles, core.graph());
+  // collect the discretization points
+  std::set<int> discretizationpts_set;
+  for (size_t i = 0; i < trueimg.rows(); ++i) {
+    for (size_t j = 0; j < trueimg.cols(); ++j) {
+      discretizationpts_set.insert(size_t(trueimg.pixel(i,j)));
+    }
+  }
+  std::vector<double> discretizationpts;
+  foreach(int i, discretizationpts_set) discretizationpts.push_back(i);
+  std::cout << "Arity: " << discretizationpts.size() << std::endl;
+  construct_graph(img, edgepot, nodepot, numparticles, core.graph(), discretizationpts);
 
 
 
@@ -591,17 +402,33 @@ int main(int argc, char** argv) {
 
   for(size_t v = 0; v < core.graph().num_vertices(); ++v) {
     const vertex_data& vdata = core.graph().vertex_data(v);
-    float a = vdata.max_asg();
+    float a = discretizationpts[vdata.belief.max_asg()];
     img.pixel(v) = a;
   }
-  std::cout << "RMSE: " << image_compare(trueimg, img) << std::endl;
 
+  double err = image_compare(trueimg, img);
+  std::cout << "RMSE: " << err << std::endl;
+  if (logfile.length() != 0) {
+    ofstream fout;
+    fout.open(logfile.c_str(), ios::app);
+    gmmfile = gmmfile.rfind("/") == std::string::npos ?
+                                       gmmfile:
+                                       gmmfile.substr(gmmfile.rfind("/")+1);
+    inputfile= inputfile.rfind("/") == std::string::npos ?
+                                       inputfile:
+                                       inputfile.substr(inputfile.rfind("/")+1);
+
+    fout << "dbp\t" << gmmfile << "\t" << inputfile << "\t" << discretizationpts.size() << "\t" << 0 << "\t" << err << "\t"
+    << runtime << std::endl;
+    fout.close();
+  }
+  
   img.save("pred_map.pgm");
 
 
   for(size_t v = 0; v < core.graph().num_vertices(); ++v) {
     const vertex_data& vdata = core.graph().vertex_data(v);
-    float a = vdata.average();
+    float a = vdata.belief.expectation();
     if (a < 0) a = 0;
     if (a > 255) a = 255;
     img.pixel(v) = (a);
