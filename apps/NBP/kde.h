3c448f4d650e392aa7874df5df26cbce031c62df
Danny Bickson <danny.bickson@gmail.com>
2010-10-18 20:06:39 -0400

    working on nbp

--- apps/NBP/kde.h
+++ apps/NBP/kde.h
@@ -10,16 +10,20 @@ typedef itpp::Mat<unsigned int> uimat;
 class kde{
 public:
 	itpp::mat centers;
-	itpp::vec bw;
+	itpp::mat bw;
         itpp::vec weights;
         uimat indices;
 
         kde(){};
 
-	kde(itpp::mat &_centers, itpp::vec &_bw, itpp::vec &_weights){
+	kde(itpp::mat &_centers, itpp::mat &_bw, itpp::vec &_weights){
 	    centers = _centers;
             bw = _bw;
             weights = _weights;
+        }
+	kde(itpp::mat &_centers, itpp::mat &_bw){
+	    centers = _centers;
+            bw = _bw;
         }
 
         kde marginal(int dim){
@@ -31,7 +35,7 @@ public:
 
         void verify(){
 	    assert(sum(weights) > 0);
-            assert(sum(bw) > 0);
+            assert(sumsum(bw) > 0);
         }
 
         int getDim(){
@@ -39,25 +43,48 @@ public:
         }
 
         // points = pts(:,ind) + getBW(npd,ind).*randKernel(getDim(npd),length(ind),getType(npd));
-         kde sample(itpp:vec ind){
+         kde sample(itpp::vec ind){
              assert(max(ind) < centers.cols());
              assert(min(ind) >= 0);
-             mat randN; 
+             itpp::mat randN; 
              itpp::randn(getDim(), ind.size(), randN);
-             mat pts = zeros(centers.rows(), ind.size());
-             mat pbw = zeros(centers.rows(), ind.size());
+             itpp::mat pts = itpp::zeros(centers.rows(), ind.size());
+             itpp::mat pbw = itpp::zeros(centers.rows(), ind.size());
              for (int i=0; i< centers.rows(); i++){
                 for (int j=0; j< ind.size(); j++){
                    pts.set(i,j,centers(i,ind(j)));
-                   pbw.set(i,j,bw(centers.bw(ind(j))));
+                   pbw.set(i,j,bw(ind(j)));
                 }
              }
-             for (int j=0; j< ind.size(); j++){
-             }
-             mat points = pts + pbw .* randN;
-             return kde(points, bpw);
+             itpp::mat points = pts + elem_mult(pbw, randN);
+             return kde(points, pbw);
                     
          }
+
+      static void matlab_print(const itpp::vec & data){
+          for (int i=0; i< data.size(); i++)
+           	std::cout<<" "<<data(i);
+      }
+
+      static void matlab_print(const itpp::mat & data){
+          std::cout<<"[";
+          for (int i=0; i< data.rows(); i++){
+             matlab_print(data.get_row(i));
+             if (i < data.rows() -1 )
+                 std::cout<<";";
+          }
+          std::cout<<"]";
+      }
+
+      void matlab_print(){
+          std::cout<<"kde("; 
+          matlab_print(centers);
+          std::cout<< ",";
+          matlab_print(bw); 
+          std::cout<< ",[";
+          matlab_print(weights);
+          std::cout << "]);" << std::endl;
+      }
        
 };
 
