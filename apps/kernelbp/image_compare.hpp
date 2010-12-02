1b4ba3239b321eb95d5bbbdc305b8f0346daf654
Yucheng Low <yuchenglow@gmail.com>
2010-12-02 00:15:15 -0500

    Removed the apps/ directory as per new App Management plan. See email titled "Re: GraphLab App Management" dated 11/22/2010 for instructions.

--- apps/kernelbp/image_compare.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef IMAGE_COMPARE_HPP
-#define IMAGE_COMPARE_HPP
-#include "image.hpp"
-double image_compare(image &trueimg, image &infered);
-#endif
--- apps/videoseg/videotomat.m
+++
@@ -1,7 +0,0 @@
-function m = videotomat(vid, numframes)
-numframes=min([numframes,vid.NumberOfFrames]);
-m = zeros([numframes, vid.Height, vid.Width]);
-
-for i = 1:numframes
-    m(i,:,:) = imfilter(rgb2gray(read(vid,i)),fspecial('gaussian'));
-end
