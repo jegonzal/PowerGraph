544c322c9b730c8c8cbb7f12a7d4033011c4a6eb
Aapo Kyrola <akyrolacmu@gmail.com>
2010-12-10 22:54:27 -0800

    Removed redundant methods.

--- extapis/java_jni/src/graphlab/Scope.java
+++ extapis/java_jni/src/graphlab/Scope.java
@@ -3,21 +3,33 @@ package graphlab;
 import java.util.List;
 
 /**
+ * Scope interface. Instance of Scope is passed to update function, and allows
+ * querying vertex neighborhood.
  * @author akyrola
  */
 public interface Scope {
-
+    
+    /**
+      * Get current vertex id.
+      */
     public int getVertexId();
-
+    
+    /**
+      * Get current vertex object.
+      */
+    Vertex getVertex();
+    
+    /**
+      * @param list of inbound edges (unmodifiable).
+      */
     public List<Edge> getInboundEdges();
 
+    /**
+      * @param list of out bound edges (unmodifiable).
+      */
     public List<Edge> getOutboundEdges();
-
-    public int[] getOutboundEdgeIds();
-
-    public int[] getInboundEdgeIds();
-
-
+ 
+    
     public Vertex getNeighbor(int vertexId);
 
     public int getNumInboundEdges();
@@ -26,5 +38,4 @@ public interface Scope {
 
     public int getNumOfVertices();
 
-    Vertex getVertex();
 }
