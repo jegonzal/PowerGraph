ed2685f65f58c13856f4fb1fea2abad17ee1b822
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2011-01-21 15:43:18 -0500

    minor formatting change

--- src/graphlab/schedulers/scheduler_list.hpp
+++ src/graphlab/schedulers/scheduler_list.hpp
@@ -5,26 +5,27 @@
 #include <iostream>
 #include <boost/preprocessor.hpp>
 
-#define __SCHEDULER_LIST__ (("chromatic", chromatic_scheduler, \
-                             "a scheduler which performs #iterations sweeps of the graph using a graph color ordering.")) \
-                            (("sweep", sweep_scheduler, \
-                              "very fast dynamic scheduler. Scans all vertices in sequence, running all update tasks on each vertex evaluated."))  \
-                            (("fifo", fifo_scheduler, \
-                              "Standard FIFO task queue, poor parallelism, but task evaluation sequence is highly predictable. Useful for debugging and testing."))  \
-                            (("priority", priority_scheduler, \
-                              "Standard Priority queue, poor parallelism, but task evaluation sequence is highly predictable. Useful for debugging"))  \
-                            (("multiqueue_fifo", multiqueue_fifo_scheduler, \
-                              "One or more FIFO task queues is assigned to each processor, where the queues are stochastically load balanced. Like the fifo scheduler, but less predictable, and much faster."))  \
-                            (("multiqueue_priority", multiqueue_priority_scheduler, \
-                              "One or more Priority task queues is assigned to each processor, where the queues are stochastically load balanced. Like the priority scheduler, but less predictable, and much faster."))  \
-                            (("splash", splash_scheduler, \
-                              "Similar to the priority queue scheduler, but allows for only one update function. Updates are evaluted in a \"splash\" ordering"))  \
-                            (("round_robin", round_robin_scheduler, \
-                              "Loops over a sequence of tasks repeatedly for # iterations."))  \
-                            (("clustered_priority", clustered_priority_scheduler, \
-                              "Like the priority scheduler, but groups vertices into clusters where the entire cluster has a single priority"))  \
-                            (("sampling", sampling_scheduler, \
-                              "A scheduler which samples vertices to update based on a multinomial probability which can be updated dynamically."))
+#define __SCHEDULER_LIST__                                              \
+  (("chromatic", chromatic_scheduler,                                   \
+    "a scheduler which performs #iterations sweeps of the graph using a graph color ordering.")) \
+  (("sweep", sweep_scheduler,                                           \
+    "very fast dynamic scheduler. Scans all vertices in sequence, running all update tasks on each vertex evaluated.")) \
+  (("fifo", fifo_scheduler,                                             \
+    "Standard FIFO task queue, poor parallelism, but task evaluation sequence is highly predictable. Useful for debugging and testing.")) \
+  (("priority", priority_scheduler,                                     \
+    "Standard Priority queue, poor parallelism, but task evaluation sequence is highly predictable. Useful for debugging")) \
+  (("multiqueue_fifo", multiqueue_fifo_scheduler,                       \
+    "One or more FIFO task queues is assigned to each processor, where the queues are stochastically load balanced. Like the fifo scheduler, but less predictable, and much faster.")) \
+  (("multiqueue_priority", multiqueue_priority_scheduler,               \
+    "One or more Priority task queues is assigned to each processor, where the queues are stochastically load balanced. Like the priority scheduler, but less predictable, and much faster.")) \
+  (("splash", splash_scheduler,                                         \
+    "Similar to the priority queue scheduler, but allows for only one update function. Updates are evaluted in a \"splash\" ordering")) \
+  (("round_robin", round_robin_scheduler,                               \
+    "Loops over a sequence of tasks repeatedly for # iterations."))     \
+  (("clustered_priority", clustered_priority_scheduler,                 \
+    "Like the priority scheduler, but groups vertices into clusters where the entire cluster has a single priority")) \
+  (("sampling", sampling_scheduler,                                     \
+    "A scheduler which samples vertices to update based on a multinomial probability which can be updated dynamically."))
 
 
 #include <graphlab/schedulers/fifo_scheduler.hpp>
