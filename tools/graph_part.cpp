ee55f2d06475cc9b7a8f2a5c57f353deb2ac427d
Yucheng Low <yuchenglow@gmail.com>
2011-02-21 22:31:57 -0500

    Graph partitioner!

--- tools/graph_part.cpp
+++ tools/graph_part.cpp
@@ -16,45 +16,102 @@
 using namespace graphlab;
   
 typedef distributed_graph<uint32_t, char> graph_type;
+double smoothing = 1;
 
-
-
-bool partition_owned_subgraph(graph_type &g) {
+void get_local_count(graph_type &g, 
+                      std::vector<size_t> &localpartcounts) {
+  foreach(vertex_id_t ownedv, g.owned_vertices()) {
+    if (g.get_vertex_data(ownedv) != uint32_t(-1)) {
+      localpartcounts[g.get_vertex_data(ownedv)]++;
+    }
+  }
+}
+ 
+bool partition_owned_subgraph(graph_type &g, 
+                              std::vector<size_t> &partcounts,
+                              std::vector<size_t> &localpartcounts) {
   bool done = true;
+  double averagepartcount = 0;
+  size_t maxpartsize = 0 ;
+  for (size_t i = 0;i < partcounts.size(); ++i) {
+    averagepartcount += partcounts[i];
+    maxpartsize = std::max<double>(maxpartsize, partcounts[i]);
+  }
+
+  averagepartcount = averagepartcount / partcounts.size();
+  if (g.rmi.procid() == 0) {
+    std::cout << "balance factor: " << maxpartsize << " / " << averagepartcount << " = "
+              << maxpartsize / averagepartcount << std::endl;
+  }
+
+  averagepartcount += smoothing;
+  boost::unordered_set<vertex_id_t> expandedthisround;  
   foreach(vertex_id_t ownedv, g.owned_vertices()) {
     if (g.get_vertex_data(ownedv) != (uint32_t)(-1)) continue;
     // figure out what to join!
-    std::map<uint32_t, uint32_t> part_to_count;
+    bool recentlyexpanded = false;
+    std::map<uint32_t, double> part_to_count;
+    // quick check for disconnected vertices
+    if (g.in_vertices(ownedv).size() == 0 && g.out_vertices(ownedv).size() == 0) {
+      // get the smallest part number
+      uint32_t smallestpart = 0;
+      uint32_t pcount = uint32_t(-1);
+      for (size_t i = 0;i < partcounts.size(); ++i) {
+        if (partcounts[i] + localpartcounts[i]  < pcount) { 
+          pcount = partcounts[i];
+          smallestpart = i;
+        }   
+      }
+      g.set_vertex_data_async(ownedv, smallestpart);
+      localpartcounts[smallestpart]++;
+      continue; 
+    }
+    // regular vertices
     foreach(vertex_id_t v, g.in_vertices(ownedv)) {
-      if (g.get_vertex_data(v) != (uint32_t)(-1)) {
-        part_to_count[g.get_vertex_data(v)]++;
+      if (expandedthisround.find(v) != expandedthisround.end()) {
+        recentlyexpanded = true;
+        break;
+      }
+      uint32_t partid = g.get_vertex_data(v);
+      if (partid != (uint32_t)(-1)) {
+        double partscore = averagepartcount / (partcounts[partid] + localpartcounts[partid] +  smoothing);
+        part_to_count[partid] += partscore;
       }
     }
-    foreach(vertex_id_t v, g.out_vertices(ownedv)) {
-      if (g.get_vertex_data(v) != (uint32_t)(-1)) {
-        part_to_count[g.get_vertex_data(v)]++;
+    foreach(vertex_id_t v, g.out_vertices(ownedv)) { 
+      if (expandedthisround.find(v) != expandedthisround.end()) {
+        recentlyexpanded = true;
+        break;
+      }
+      uint32_t partid = g.get_vertex_data(v);
+      if (partid != (uint32_t)(-1)) {
+        double partscore = averagepartcount / (partcounts[partid] + localpartcounts[partid] + smoothing);
+        part_to_count[partid] += partscore;
       }
     }
     // fast quit
-    if (part_to_count.size() == 0) {
+    if (recentlyexpanded || part_to_count.size() == 0) {
       done = false;
       continue;
     }
     
     // find the maximum part
-    typedef std::map<uint32_t, uint32_t>::value_type ptype;
-    uint32_t bestpart = (uint32_t)(-1);
-    uint32_t bestpartcount = 0;
+    typedef std::map<uint32_t, double>::value_type ptype;
+    uint32_t bestpart = uint32_t(-1);
+    double bestpartscore = 0.0;
     foreach(const ptype& p, part_to_count) {
-      if (p.second > bestpartcount) {
-        bestpartcount = p.second;
+      if (p.second > bestpartscore) {
+        bestpartscore = p.second;
         bestpart = p.first;
       }
     }
-    if (bestpart == (uint32_t)(-1)) {
+    if (bestpart == uint32_t(-1)) {
       done = false;
       continue;
     }
+    // pick a dd
+    expandedthisround.insert(ownedv);
+    localpartcounts[bestpart]++;
     g.set_vertex_data_async(ownedv, bestpart);
   }
   return done;
@@ -101,9 +158,10 @@ int main(int argc, char** argv) {
 
   graph_type dg(dc, path, true);
   size_t iter = 0;
+  dc.barrier();
   // set all vertex data to -1
   foreach(vertex_id_t ownedv, dg.owned_vertices()) {
-    dg.set_vertex_data_async(ownedv, uint32_t(-1));
+    dg.set_vertex_data(ownedv, uint32_t(-1));
   }
   // synchronize all ghosts
   dg.rmi.full_barrier();
@@ -116,18 +174,39 @@ int main(int argc, char** argv) {
       do {
         v = graphlab::random::rand_int(dg.num_vertices() - 1);
       }while(seeds.find(v) != seeds.end());
+      seeds.insert(v);
       dg.set_vertex_data(v, i);
     }
   }
   
   dc.full_barrier();
-  
+
+  std::vector<size_t> localpartcounts(numparts, 0);
+  std::vector<size_t> globalpartcounts(numparts, 1);
+  std::vector<std::vector<size_t> > allgathertarget(dc.numprocs()); 
+  get_local_count(dg, localpartcounts);
   while(1) {
     iter++;
-    bool c = partition_owned_subgraph(dg);
+    bool c = partition_owned_subgraph(dg, globalpartcounts, localpartcounts);
     std::vector<int> vcal(dc.numprocs(), 0);
     vcal[dc.procid()] = c;
     dc.all_gather(vcal);
+
+    allgathertarget[dc.procid()] = localpartcounts;
+    dc.all_gather(allgathertarget);
+    
+    globalpartcounts = std::vector<size_t>(numparts, 0);
+    for (size_t i = 0;i <dc.numprocs(); ++i) {
+      for (size_t j = 0; j < numparts; ++j) { 
+        globalpartcounts[j] += allgathertarget[i][j]; 
+      }
+    }
+    if (dc.procid() == 0) {
+      for (size_t i = 0; i < numparts; ++i) {
+        std::cout << globalpartcounts[i] << " ";
+      }
+      std::cout << std::endl;
+    }
     // check if done
     bool isdone = check_is_done(vcal);
     if (isdone) break;
