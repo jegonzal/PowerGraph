431bbbbb1d96742a2d3c36742f68068dcdfba9c4
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2011-02-27 13:21:21 -0500

    formatting chagnes for readability (emacs reindent)

--- src/graphlab/serialization/serializable_pod.hpp
+++ src/graphlab/serialization/serializable_pod.hpp
@@ -6,20 +6,20 @@
 // that will lose some cross platform support
 // in particular, all platforms will need sizeof(struct) to be identical
 
-#define SERIALIZABLE_POD(tname) \
-  namespace graphlab { namespace archive_detail {             \
-  template <typename ArcType> struct serialize_impl<ArcType, tname>{                        \
-    static void exec(ArcType &a, const tname &i) {              \
-    a.o->write(reinterpret_cast<const char*>(&i), sizeof(tname));\
-    }                                                         \
-  };                                                          \
-  template <typename ArcType> struct deserialize_impl<ArcType, tname>{                      \
-    static void exec(ArcType &a, tname &t) {                    \
-    a.i->read(reinterpret_cast<char*>(&t), sizeof(tname)); \
-    }                                                         \
-  };                                                          \
-  __GEN_FAST_VECTOR_SERIALIZE__(tname)                       \
-  }}
+#define SERIALIZABLE_POD(tname)                                         \
+  namespace graphlab { namespace archive_detail {                       \
+      template <typename ArcType> struct serialize_impl<ArcType, tname>{ \
+        static void exec(ArcType &a, const tname &i) {                  \
+          a.o->write(reinterpret_cast<const char*>(&i), sizeof(tname)); \
+        }                                                               \
+      };                                                                \
+      template <typename ArcType> struct deserialize_impl<ArcType, tname>{ \
+        static void exec(ArcType &a, tname &t) {                        \
+          a.i->read(reinterpret_cast<char*>(&t), sizeof(tname));        \
+        }                                                               \
+      };                                                                \
+      __GEN_FAST_VECTOR_SERIALIZE__(tname)                              \
+    }}
 
 #endif
 
