bf61ea7598b7b8efd97b4b78bce2630e21202545
Danny Bickson <danny.bickson@gmail.com>
2011-06-28 11:55:22 -0400

    changed wals to remove call to diag()

--- demoapps/pmf/pmf.cpp
+++ demoapps/pmf/pmf.cpp
@@ -1352,19 +1352,19 @@ void load_pmf_graph(const char* filename, graph_type * _g, testtype data_type,gl
   assert(rc==4); 
   assert(_K>=1);
   assert(_M>=1 && _N>=1); 
-  if (data_type != TRAINING && M != _M)
+  if (data_type == TRAINING){
+  	M=_M; N= _N; K= _K;
+	if (infile == "kddcup" || infile == "kddcup2")// DB: ugly - kdd cup data has more time bins for test data than in training data. can fix this buy setting the time bins in training data to 6649.
+		K=6649;
+     K=ceil((K-truncating)/scaling);
+  }
+
+ if (data_type != TRAINING && M != _M)
 	logstream(LOG_WARNING) << " wrong number of users: " << _M << " instead of " << M << " in " << testtypename[data_type] << std::endl;
   if (data_type != TRAINING && N != _N)
 	logstream(LOG_WARNING) << " wrong number of movies: " << _N << " instead of " << N << " in " << testtypename[data_type] << std::endl;
   if (data_type != TRAINING && K != _K)
 	logstream(LOG_WARNING) << " wrong number of time bins: " << _K << " instead of " << K << " in " << testtypename[data_type] <<std::endl;
-
-  if (data_type == TRAINING){
-  	M=_M; N= _N; K= _K;
-	if (infile == "kddcup" || infile == "kddcup2")// DB: ugly - kdd cup data has more time bins for test data than in training data. can fix this buy setting the time bins in training data to 6649.
-		K=6649;
-        K=ceil((K-truncating)/scaling);
-  }
 
   //if (data_type==TRAINING)
   printf("Matrix size is: USERS %d MOVIES %d TIME BINS %d\n", M, N, K);
