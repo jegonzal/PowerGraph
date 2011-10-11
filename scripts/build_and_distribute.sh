6d362b0154e53caee84ce0c1442faddefc64b062
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2011-10-11 19:53:54 +0000

    building and distribute script for ec2

---
+++ scripts/build_and_distribute.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/jegonzal/graphlabapi/release/apps/dist2_apps/webgraph_bp
+make -j8
+cp * ~/bin
+
+for i in `cat ~/hosts`; do
+    echo "connecting to $i";
+    rsync -avz ~/bin $i:~/.;
+done
