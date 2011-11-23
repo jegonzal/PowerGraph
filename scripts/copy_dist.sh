ca15090e69dc1c0572b07edc1bd4a766a0aa5a78
Danny Bickson <danny.bickson@gmail.com>
2011-11-23 02:01:41 -0500

    added overflow detection when using k-means fuzzy with small exponent

--- scripts/copy_dist.sh
+++ scripts/copy_dist.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cp dist/graphlabapi_v1_$1.tar.gz ~/www-graphlab/graphlab/release/
 svn add ~/www-graphlab/graphlab/release/graphlabapi_v1_$1.tar.gz
-svn commit -m "new release graphlabapi_v1_$1.tar.gz" ~/www-graphlab/graphlab
+vim ~/www-graphlab/graphlab/download.html
+cd ~/www-graphlab/graphlab/
+svn commit -m "new release graphlabapi_v1_$1.tar.gz" 
+
