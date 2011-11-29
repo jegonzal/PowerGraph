9692b5d1e77d9710f566c4950620d546b9bdf045
Yucheng Low <yuchenglow@gmail.com>
2011-11-29 06:54:34 -0500

    more informative integer compressino benchmark

--- tests/integercompress_bench.cpp
+++ tests/integercompress_bench.cpp
@@ -17,15 +17,19 @@ int main(int argc, char** argv) {
   char c[10];
   int64_t u2;
   
-  std::cout << "compressing and decompressing range [-10000000, 100000000]" << std::endl;
+
   ti.start();
   size_t totallen = 0;
-  for (int64_t u = -10000000;u <= 100000000; ++u) {
+  int64_t rangelow = -100000000;
+  int64_t rangehigh = 100000000;
+  std::cout << "compressing and decompressing range [" << rangelow << ", "<< rangehigh << "]" << std::endl;
+  for (int64_t u = rangelow;u <= rangehigh; ++u) {
     unsigned char len = compress_int(u, c);
     totallen += len;
     decompress_int(c + 10 - len, u2);
   }
   std::cout << "runtime: " << ti.current_time() << std::endl;
-  std::cout << "accumulated length: " << totallen << std::endl;
+  std::cout << "Range compression rate: " << (8 * (rangehigh - rangelow) / ti.current_time())/(1024 * 1024) << " MBps" << std::endl;
+  std::cout << "Compression Ratio: " << (double)totallen / (8 * (rangehigh - rangelow)) << std::endl;
 
 }
