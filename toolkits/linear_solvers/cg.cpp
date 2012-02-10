2dce0bf071807d4c19137bc269e64c283a6463c4
Danny Bickson <danny.bickson@gmail.com>
2012-02-10 09:04:36 -0500

    added conjugate gradient implementation on big math implementation

---
+++ toolkits/linear_solvers/cg.cpp
@@ -0,0 +1,265 @@
+/**  
+ * Copyright (c) 2009 Carnegie Mellon University. 
+ *     All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS
+ *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied.  See the License for the specific language
+ *  governing permissions and limitations under the License.
+ *
+ * For more about this software visit:
+ *
+ *      http://www.graphlab.ml.cmu.edu
+ *
+ */
+
+
+
+#include <graphlab.hpp>
+#include "graphlab.hpp"
+#include "../shared/io.hpp"
+#include "../shared/types.hpp"
+#include "../shared/mathlayer.hpp"
+
+using namespace graphlab;
+double regularization = 0;
+bool calc_residual = false;
+bool debug = false;
+int max_iter = 10;
+
+struct vertex_data {
+
+  vec pvec;
+  double A_ii;
+  vertex_data() : A_ii(1) {
+  }
+  void add_self_edge(double value) { A_ii = value + regularization; }
+
+  void set_val(double value, int field_type) { 
+    pvec[field_type] = value;
+  }
+  //only one output for jacobi - solution x
+  double get_output(int field_type){ return pvec[field_type]; }
+}; // end of vertex_data
+
+struct edge_data {
+  real_type weight;
+  edge_data(double weight = 0) : weight(weight) { }
+};
+
+
+typedef graphlab::graph<vertex_data, edge_data> graph_type;
+#include "../shared/math.hpp"
+
+/*function [x] = conjgrad(A,b,x)
+    r=b-A*x; ///DIST
+    p=r;     //SER
+    rsold=r'*r;  //SER
+
+    for i=1:size(A,1)
+        Ap=A*p;               //DIST
+        alpha=rsold/(p'*Ap);  //SER
+        x=x+alpha*p;          //SER
+        r=r-alpha*Ap;         //SER
+        rsnew=r'*r;           //SER
+        if sqrt(rsnew)<1e-10  //SER
+              break;
+        end
+        p=r+rsnew/rsold*p;    //SER
+        rsold=rsnew;          //SER
+    end
+end
+*/
+enum input_pos{
+  CG_Y = 0,
+  CG_PREC = 1,
+  CG_R = 2,
+  CG_P = 3,
+  CG_X = 4,
+  CG_AP = 5,
+  CG_T = 6
+};
+
+int main(int argc,  char *argv[]) {
+  
+  global_logger().set_log_level(LOG_INFO);
+  global_logger().set_log_to_console(true);
+
+  graphlab::command_line_options clopts("GraphLab Linear Solver Library");
+
+  std::string datafile, yfile, xfile;
+  std::string format = "matrixmarket";
+  real_type threshold = 1e-5;
+  size_t sync_interval = 10000;
+  int unittest = 0;
+
+  clopts.attach_option("data", &datafile, datafile,
+                       "matrix A input file");
+  clopts.add_positional("data");
+  clopts.attach_option("yfile", &yfile, yfile,
+                       "vector y input file");
+  clopts.attach_option("xfile", &xfile, xfile,
+                       "vector x input file (optional)");
+  clopts.attach_option("threshold", &threshold, threshold, "termination threshold.");
+  clopts.add_positional("threshold");
+  clopts.attach_option("format", &format, format, "matrix format");
+  clopts.attach_option("debug", &debug, debug, "Display debug output.");
+  clopts.attach_option("syncinterval", 
+                       &sync_interval, sync_interval, 
+                       "sync interval (number of update functions before convergen detection");
+  clopts.attach_option("regularization", &regularization, regularization, 
+	               "regularization added to the main diagonal");
+  clopts.attach_option("unittest", &unittest, unittest, 
+		       "unit testing 0=None, 1=3x3 matrix");
+  clopts.attach_option("max_iter", &max_iter, max_iter, "max number of iterations");
+  clopts.attach_option("calc_residual", &calc_residual, calc_residual, "calc residual in each iteration"); 
+
+  // Parse the command line arguments
+  if(!clopts.parse(argc, argv)) {
+    std::cout << "Invalid arguments!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  logstream(LOG_WARNING)
+    << "Eigen detected. (This is actually good news!)" << std::endl;
+  logstream(LOG_INFO) 
+    << "GraphLab Linear solver library code by Danny Bickson, CMU" 
+    << std::endl 
+    << "Send comments and bug reports to danny.bickson@gmail.com" 
+    << std::endl 
+    << "Currently implemented algorithms are: Gaussian Belief Propagation, "
+    << "Jacobi method, Conjugate Gradient" << std::endl;
+
+
+
+  // Create a core
+  graphlab::core<graph_type, Axb> core;
+  core.set_options(clopts); // Set the engine options
+
+  //unit testing
+  if (unittest == 1){
+/*A= [  1.8147    0.9134    0.2785
+       0.9058    1.6324    0.5469
+       0.1270    0.0975    1.9575 ];
+
+  y= [ 0.9649    0.1576    0.9706 ]';
+  x= [ 0.6803   -0.4396    0.4736 ]';
+*/
+    datafile = "A"; yfile = "y"; xfile = "x"; sync_interval = 120;
+  }
+
+  std::cout << "Load matrix A" << std::endl;
+  bipartite_graph_descriptor matrix_info;
+  load_graph(datafile, format, matrix_info, core.graph());
+  std::cout << "Load Y values" << std::endl;
+  load_vector(yfile, format, matrix_info, core.graph(), CG_X, false);
+  std::cout << "Load x values" << std::endl;
+  load_vector(xfile, format, matrix_info, core.graph(), CG_Y, true);
+  
+  math_info mi;
+  //aggregator acum;
+  //core.add_aggregator("sync", acum, sync_interval);
+  //core.add_global("REAL_NORM", double(0));
+  //core.add_global("RELATIVE_NORM", double(0));
+  //core.add_global("THRESHOLD", threshold); 
+
+  
+  //  int tmp=ps.n; ps.n=ps.m; ps.m = tmp;
+  //  diff = NAN;
+  //  init_row_cols();
+
+    DistMat A(matrix_info,mi);
+    DistVec b(mi, matrix_info, CG_Y,true, "b");
+    DistVec prec(mi, matrix_info, CG_PREC,true, "prec");
+    DistVec r(mi, matrix_info, CG_R, true, "r");
+    DistVec p(mi, matrix_info, CG_P,true, "p");
+    DistVec x(mi, matrix_info, CG_X,true, "x");
+    DistVec Ap(mi, matrix_info, CG_AP, false, "Ap");
+    DistVec t(mi, matrix_info, CG_T,true, "t");
+    //initialize startng guess
+
+    vec init_x;
+    if (debug)
+      init_x = ones(matrix_info.num_nodes(false));
+    x = init_x;
+
+    DistDouble rsold(mi), rnew(mi), alpha(mi);
+
+    /* r = -A*x+b;
+       if (~sqaure)
+         r=A'*r;
+       end
+       p = r;
+       rsold = r'*r;
+    */
+    r=-A*x+b; 
+    if (!matrix_info.is_square())
+      r=A._transpose()*r;
+    p = r;
+    rsold = r._transpose()*r;
+
+     /*
+     for i=1:size(A,1)
+        Ap=A*p; 
+        if (~matrix_info.is_square())
+          Ap=A'*Ap; 
+        end             
+        alpha=rsold/(p'*Ap); 
+        x=x+alpha*p;        
+        r=r-alpha*Ap;      
+        rsnew=r'*r;       
+        if sqrt(rsnew)<1e-10 
+              break;
+        end
+        p=r+rsnew/rsold*p;  
+        rsold=rsnew;       
+    end
+    */
+
+    for (int i=1; i <= std::min(max_iter,size(A,1)); i++){
+        Ap=A*p;
+        if (!matrix_info.is_square())
+          Ap= A._transpose()*Ap;
+        alpha = rsold/(p._transpose()*Ap);
+        x=x+alpha*p;
+   
+        if (calc_residual){
+          t=A*x-b;
+          logstream(LOG_INFO)<<"Iteration " << i << " approximated solution residual is " << norm(t).toDouble() << std::endl;
+        }
+
+        r=r-alpha*Ap;
+        rnew=r._transpose()*r;
+        if (sqrt(rnew)< threshold){
+          double diff = sqrt(rnew).toDouble();
+          logstream(LOG_INFO)<<" Conjugate gradient converged in iteration "<<i<<" to an accuracy of "  << diff << std::endl; 
+          break;
+        }
+        p=r+(rnew/rsold)*p;
+        rsold=rnew;
+    }
+
+  
+  vec ret = fill_output(&core.graph(), matrix_info, CG_X);
+  write_output_vector(datafile + "x.out", format, ret, false);
+
+
+  if (unittest == 1){
+    double real_norm = core.get_global<double>("REAL_NORM");
+    double relative_norm = core.get_global<double>("RELATIVE_NORM");
+    assert(real_norm < 1e-30);
+    assert(relative_norm < 1e-30);
+  }
+
+   return EXIT_SUCCESS;
+}
+
+
+
---
+++ toolkits/shared/math.hpp
@@ -0,0 +1,426 @@
+/**  
+ * Copyright (c) 2009 Carnegie Mellon University. 
+ *     All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS
+ *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied.  See the License for the specific language
+ *  governing permissions and limitations under the License.
+ *
+ * For more about this software visit:
+ *
+ *      http://www.graphlab.ml.cmu.edu
+ *
+ */
+
+
+#ifndef _MATH_HPP
+#define _MATH_HPP
+
+#include "types.hpp"
+#include "mathlayer.hpp"
+#include "graphlab.hpp"
+
+
+extern double regularization;
+extern bool debug;
+
+struct math_info{
+  int increment;
+  double  c;
+  double  d;
+  int x_offset, b_offset , y_offset, r_offset;
+  bool A_offset;
+  std::vector<std::string> names;
+
+  math_info(){
+    reset_offsets();
+  }
+
+  void reset_offsets(){
+    increment = 2;
+    c=1.0; d=0.0;
+    x_offset = b_offset = y_offset = r_offset = -1;
+    A_offset = false;
+  }
+  int increment_offset(){
+    return increment++;
+  }
+
+
+};
+
+
+bipartite_graph_descriptor info;
+math_info mi;
+
+#define MAX_PRINT_ITEMS 21
+#define MAX_OFFSET 7
+double runtime = 0;
+
+using namespace graphlab;
+//std::vector<vertex_id_t> rows,cols;
+
+typedef graph<vertex_data,edge_data>::edge_list_type edge_list_type;
+typedef graph<vertex_data,edge_data>::edge_type edge_type;
+
+graph_type * pgraph;
+
+void init_math(graph_type * _pgraph){
+  pgraph = _pgraph;
+}
+
+/***
+ * UPDATE FUNCTION (ROWS)
+ */
+struct Axb:
+ public iupdate_functor<graph_type, Axb> {
+ 
+  void operator()(icontext_type &context){
+
+  vertex_data& user = context.vertex_data();
+  bool rows = context.vertex_id() < (uint)info.get_start_node(false);
+  double * pr = (double*)&user;
+  assert(mi.r_offset >=0);
+  double val = 0;
+  assert(mi.x_offset >=0 || mi.y_offset>=0);
+  timer t; t.start();
+  
+  /*** COMPUTE r = c*A*x  ********/
+  if (mi.A_offset  && mi.x_offset >= 0){
+   edge_list_type edges = rows?
+	context.out_edges() : context.in_edges(); 
+   for (size_t i = 0; i < edges.size(); i++){
+      const edge_data & edge = context.edge_data(edges[i]);
+      const vertex_data  & movie = context.const_vertex_data(rows ? edges[i].target() : edges[i].source());
+      double * px = (double*)&movie.pvec[0];
+      val += (mi.c * edge.weight * px[mi.x_offset]);
+    }
+  
+    if (info.is_square())// add the diagonal term
+      val += (mi.c* (user.A_ii+ regularization) * pr[mi.x_offset]);
+  }
+ /***** COMPUTE r = c*I*x  *****/
+  else if (!mi.A_offset && mi.x_offset >= 0){
+     val = mi.c*pr[mi.x_offset];
+  }
+  
+  /**** COMPUTE r+= d*y (optional) ***/
+  if (mi.y_offset>= 0){
+    val += mi.d*pr[mi.y_offset]; 
+  }
+
+  pr[mi.r_offset] = val;
+}
+};
+
+
+
+class DistMat; 
+class DistDouble;
+
+class DistVec{
+   public:
+   int offset;
+   std::string name; //optional
+   bool transpose;
+   bipartite_graph_descriptor info;
+   int start; 
+   int end;
+
+   void init(){
+     debug_print(name);
+     start = info.get_start_node(!transpose);
+     end = info.get_end_node(!transpose);
+     assert(start < end);
+   };
+
+   DistVec(math_info &_mi, bipartite_graph_descriptor &_info, int _offset, bool _transpose, const std::string & _name){
+     offset = _offset;
+     name = _name;
+     mi = _mi;
+     info = _info;
+     transpose = _transpose;
+     init();
+   }
+
+   DistVec& operator-(){
+     mi.d=-1.0;
+     return *this; 
+   }
+   DistVec& operator-(const DistVec & other){
+     mi.x_offset = offset;
+     mi.y_offset = other.offset;
+     transpose = other.transpose;
+     if (mi.d == 0)
+       mi.d = -1.0;
+     else 
+       mi.d*=-1.0;
+     return *this;
+   }
+   DistVec& operator+(){
+     if (mi.d == 0)
+       mi.d=1.0;
+     return *this;
+   }
+   DistVec& operator+(const DistVec &other){
+      mi.x_offset =offset;
+      mi.y_offset = other.offset;
+      transpose = other.transpose;
+      return *this; 
+   }
+
+   DistVec& operator=(const DistVec & vec){
+     assert(offset < MAX_OFFSET);
+     if (mi.x_offset == -1 && mi.y_offset == -1){
+         mi.y_offset = vec.offset;
+       }  
+      mi.r_offset = offset;
+      if (mi.d == 0.0)
+        mi.d=1.0;
+      transpose = vec.transpose;
+      //glcore->add_tasks((!transpose)?rows:cols, Axb, 1); //TODO
+      //runtime += glcore->start();
+      debug_print(name);
+      mi.reset_offsets();
+      return *this;
+  }
+
+  DistVec& operator=(const vec & pvec){
+    assert(offset >= 0);
+    assert(pvec.size() == info.num_nodes(true) || pvec.size() == info.num_nodes(false));
+    if (!info.is_square() && pvec.size() == info.num_nodes(false)){
+      transpose = true;
+    }
+    else {
+      transpose = false;
+    }
+#pragma omp parallel for    
+    for (int i=start; i< end; i++){  
+         const vertex_data * data = &pgraph->vertex_data(i);
+         double * pv = (double*)&data->pvec[0];
+         pv[offset] = pvec[i-start];  
+    }
+    debug_print(name);
+    return *this;       
+  }
+
+
+  void to_vec(vec & v){
+    if (v.size() == 0){
+      v.resize(info.num_nodes(!transpose));
+    }
+    int start = info.get_start_node(!transpose);    
+    for (int i=start; i<info.get_end_node(!transpose); i++){
+         const vertex_data * data = &pgraph->vertex_data(i);        
+	 double * pv = (double*)data;
+         v[i-start] = pv[offset];
+     }
+  }
+
+  void debug_print(const char * name){
+     if (debug){
+       std::cout<<name<<" ("<<name<<" "<<offset<<" [ " << info.num_nodes(!transpose) << "] ";
+       for (int i=start; i< std::min(info.get_end_node(!transpose), info.get_start_node(!transpose)+MAX_PRINT_ITEMS); i++){  //TODO
+         const vertex_data * data = &pgraph->vertex_data(i);
+         double * pv = (double*)data;
+         std::cout<<pv[mi.r_offset==-1?offset:mi.r_offset]<<" ";
+       }
+       std::cout<<std::endl;
+     }
+  }
+  void debug_print(std::string name){ return debug_print(name.c_str());}
+
+  DistDouble operator*(const DistVec & other);
+  
+  DistVec& operator*(const double val){
+     mi.d=val;
+     return *this;
+  }
+  DistVec& _transpose() { 
+     /*if (!config.square){
+       start = n; end = m+n;
+     }*/
+     return *this;
+  }
+
+  DistVec& operator=(DistMat &mat);
+ 
+ };
+
+
+
+/*
+ * wrapper for computing r = c*A*x+d*b*y
+ */
+class DistMat{
+  public:
+    bool transpose;
+    bipartite_graph_descriptor info;
+    math_info mi;
+
+    DistMat(bipartite_graph_descriptor& _info, math_info & _mi) { 
+      info = _info;
+      mi = _mi;
+      transpose = false;
+    };
+
+
+    DistMat &operator*(DistVec & v){
+      	mi.x_offset = v.offset;
+        mi.A_offset = true;
+        //v.transpose = transpose;
+        //r_offset = A_offset;
+        return *this;
+    }
+    DistMat &operator-(){
+        mi.c=-1.0;
+        return *this;
+    }
+    
+    DistMat &operator+(){
+        mi.c=1.0;
+        return *this;
+    }
+    DistMat &operator+(const DistVec &v){
+        mi.y_offset = v.offset;
+        if (mi.d == 0.0)
+            mi.d=1.0;
+        return *this;
+    }
+    DistMat &operator-(const DistVec &v){
+        mi.y_offset = v.offset;
+        if (mi.d == 0.0)
+          mi.d=-1.0;
+        else 
+          mi.d*=-1.0;
+        return *this;
+    }
+    DistMat & _transpose(){
+       transpose = true;
+       return *this;
+    }
+   
+};
+
+DistVec& DistVec::operator=(DistMat &mat){
+  mi.r_offset = offset;
+  transpose = mat.transpose;
+ // glcore->add_tasks((!mi.transpose)?rows:cols, Axb, 1);//TODO
+ // runtime += glcore->start();
+  debug_print(name);
+  mi.reset_offsets();
+  mat.transpose = false;
+  return *this;
+}
+
+
+class DistDouble{
+  public:
+     double val;
+     std::string name;
+     math_info mi;
+
+     DistDouble(math_info & _mi): mi(_mi) {};
+   
+     const DistVec& operator*(const DistVec & dval){
+        mi.d=val;
+        return dval;
+     }
+     DistDouble  operator/(const DistDouble dval){
+        DistDouble mval(mi);
+        mval.val = val / dval.val;
+        return mval;
+     }
+     bool operator<(const double other){
+         return val < other;
+     }
+     DistDouble & operator=(const DistDouble & other){
+         val = other.val;
+         debug_print(name);
+         return *this;
+     }
+     void debug_print(const char * name){
+       std::cout<<name<<" "<<val<<std::endl;
+     }
+     double toDouble(){
+        return val;
+     }
+     void debug_print(std::string name){ return debug_print(name.c_str()); }
+
+
+ };
+
+ DistDouble DistVec::operator*(const DistVec & vec){
+      mi.y_offset = offset;
+      mi.b_offset = vec.offset;
+      if (mi.d == 0) 
+        mi.d = 1.0;
+      assert(mi.y_offset >=0 && mi.b_offset >= 0);
+
+      double val = 0;
+      for (int i=start; i< end; i++){  
+         const vertex_data * data = &pgraph->vertex_data(i);
+         double * pv = (double*)&data->pvec[0];
+        // if (y_offset >= 0 && b_offset == -1)
+	     //val += pv[y_offset] * pv[y_offset];
+         val += mi.d* pv[mi.y_offset] * pv[mi.b_offset];
+      }
+      mi.reset_offsets();
+      DistDouble mval(mi);
+      mval.val = val;
+      return mval;
+ }
+
+
+int size(DistMat & A, int pos){
+   assert(pos == 1 || pos == 2);
+   return A.info.num_nodes(!A.transpose);
+}
+
+DistDouble sqrt(DistDouble & dval){
+    DistDouble mval(mi);
+    mval.val=sqrt(dval.val);
+    return mval;
+}
+
+DistDouble norm(DistVec & vec){
+    assert(vec.offset>=0);
+    assert(vec.start < vec.end);
+
+    DistDouble mval(mi);
+    mval.val = 0;
+    for (int i=vec.start; i < vec.end; i++){
+       const vertex_data * data = &pgraph->vertex_data(i);
+       double * px = (double*)&data->pvec[0];
+       mval.val += px[vec.offset]*px[vec.offset];
+    }
+    mval.val = sqrt(mval.val);
+    return mval;
+}
+
+void init_row_cols(){
+     /* 
+    for (int i=0; i< (int)info.get_start_node(false); i++)
+      rows.push_back(i);
+
+    if (config.square){
+      cols = rows;
+    }
+    else {
+      assert(ps.m!= 0);
+      for (int i=info.get_start_node(false); i < (int)info.get_end_node(false); i++)
+        cols.push_back(i);
+    }*/
+//TODO
+}
+
+
+#endif //_MATH_HPP
