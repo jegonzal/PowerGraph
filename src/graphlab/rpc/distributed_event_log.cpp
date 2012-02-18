1413d803a642fc0b476463993aa81992d0a50487
Yucheng Low <yuchenglow@gmail.com>
2012-02-17 22:24:54 -0500

    - distributed event log  - fixed bug in distributed engine

---
+++ src/graphlab/rpc/distributed_event_log.cpp
@@ -0,0 +1,245 @@
+#include <limits>
+#include <graphlab/rpc/distributed_event_log.hpp>
+#include <graphlab/util/timer.hpp>
+#include <graphlab/logger/assertions.hpp>
+
+#define EVENT_BAR_WIDTH 40
+
+namespace graphlab {
+
+  
+void dist_event_log::initialize(distributed_control& dc,
+                           std::ostream &ostrm,
+                           size_t flush_interval_ms,
+                           event_print_type event_print) {
+  rmi = new dc_dist_object<dist_event_log>(dc, this);
+  rmi->barrier();
+  m.lock();
+  out = &ostrm;
+  flush_interval = flush_interval_ms;
+  if (rmi->procid() > 0) {
+    flush_interval /= 5;
+  }
+  print_method = event_print;
+  prevtime = 0;
+  ti.start();
+  cond.signal();
+  std::cout << "init" << std::endl;
+  m.unlock();
+}
+
+dist_event_log::~dist_event_log() {
+  finished = true;
+  m.lock();
+  cond.signal();
+  m.unlock();
+  printing_thread.join();
+  delete rmi;
+}
+
+void dist_event_log::close() {
+  out = NULL;
+  m.lock();
+  flush_interval = 0;
+  m.unlock();
+}
+
+void dist_event_log::add_event_type(unsigned char eventid,
+                               std::string description) {
+  descriptions[eventid] = description;
+  max_desc_length = std::max(max_desc_length, description.length());
+  ASSERT_MSG(max_desc_length <= 30, "Event Description length must be <= 30 characters");
+  counters[eventid].value = 0;
+  hascounter.set_bit(eventid);
+  if (rmi->procid() == 0) {
+    globalcounters[eventid].resize(rmi->numprocs());
+  }
+}
+
+void dist_event_log::accumulate_event_aggregator(procid_t proc,
+                                                 unsigned char eventid,
+                                                 size_t count) {
+  hasevents = true;
+  globalcounters[eventid][proc].inc(count);
+}
+  
+struct counter_statistics{
+  size_t minimum;
+  size_t maximum;
+  size_t average;
+  size_t total;
+};
+  
+static void compute_statistics(std::vector<atomic<size_t> > &vec,
+                               counter_statistics& c) {
+  c.minimum = std::numeric_limits<size_t>::max();
+  c.maximum = std::numeric_limits<size_t>::min();
+  c.average = 0;
+  c.total = 0;
+  for (size_t i = 0; i < vec.size(); ++i) {
+    size_t ctr = vec[i].exchange(0);
+    c.minimum = std::min(c.minimum, ctr);
+    c.maximum = std::max(c.minimum, ctr);
+    c.total += ctr;
+  }
+  c.average = c.total / vec.size();
+}
+
+
+
+static void print_bar(std::ostream& out,
+                             size_t val,
+                             size_t len) {
+  // compute the bar lengths
+  if (len == 0) return;
+  val = val * EVENT_BAR_WIDTH / len;
+  if (val > EVENT_BAR_WIDTH) val = EVENT_BAR_WIDTH;
+  size_t i = 0;
+  for (i = 0; i < val; ++i) out.put('#');
+  for (; i < EVENT_BAR_WIDTH; ++i) out.put(' ');
+}
+
+static void print_triple_bar(std::ostream& out,
+                             size_t low,
+                             size_t mid,
+                             size_t high,
+                             size_t len) {
+  // compute the bar lengths
+  if (len == 0) return;
+  low = low * EVENT_BAR_WIDTH / len;
+  if (low > EVENT_BAR_WIDTH) low = EVENT_BAR_WIDTH;
+
+  mid = mid * EVENT_BAR_WIDTH / len;
+  if (mid > EVENT_BAR_WIDTH) mid = EVENT_BAR_WIDTH;
+  
+  high = high * EVENT_BAR_WIDTH / len;
+  if (high > EVENT_BAR_WIDTH) high = EVENT_BAR_WIDTH;
+  size_t i = 0;
+  for (i = 0; i < low; ++i) out.put('-');
+  for (; i < mid; ++i) out.put('*');
+  for (; i < high; ++i) out.put('#');
+  for (; i < EVENT_BAR_WIDTH; ++i) out.put(' ');
+}
+  
+void dist_event_log::print_log() {
+  uint32_t pos;
+  if (!hascounter.first_bit(pos)) return;
+  double curtime = ti.current_time_millis();
+  double timegap = curtime - prevtime;
+  prevtime = curtime;
+
+  if (hasevents == false && noeventctr == 1) return;
+
+  counter_statistics stats[256];
+  // accumulate the statistics for printing
+  do {
+    compute_statistics(globalcounters[pos], stats[pos]);
+  } while(hascounter.next_bit(pos));
+  
+  bool found_events = false;
+  (*out) << "Time: " << "+"<<timegap << "\t" << curtime << "\n";
+  
+  // reset the counter
+  hascounter.first_bit(pos);
+  if (print_method == NUMBER) {
+    do {
+      found_events = found_events || stats[pos].total > 0;
+      (*out) << pos  << ":\t" << stats[pos].minimum << "\t"
+             << stats[pos].average << "\t" << stats[pos].maximum << "\t"
+             << stats[pos].total << "\t" << 1000 * stats[pos].total / timegap << " /s\n";
+    } while(hascounter.next_bit(pos));
+  }
+  else if (print_method == DESCRIPTION) {
+    do {
+      found_events = found_events || stats[pos].total > 0;
+      (*out) << descriptions[pos]  << ":\t" << stats[pos].minimum << "\t"
+             << stats[pos].average << "\t" << stats[pos].maximum << "\t"
+             << stats[pos].total << "\t" << 1000 * stats[pos].total / timegap << " /s\n";
+    } while(hascounter.next_bit(pos));
+  }
+  else if (print_method == RATE_BAR) {
+    char spacebuf[60];
+    memset(spacebuf, ' ', EVENT_BAR_WIDTH);
+    do {
+      found_events = found_events || stats[pos].total > 0;
+      maxcounter[pos] = std::max(maxcounter[pos], stats[pos].total);
+      maxproc_counter[pos] = std::max(maxcounter[pos], stats[pos].maximum);
+      
+      // print the description prefix
+      spacebuf[max_desc_length - descriptions[pos].length() + 1] = 0;
+      (*out) << descriptions[pos]  << spacebuf << "|";
+
+      print_triple_bar(*out,
+                       stats[pos].minimum,
+                       stats[pos].average,
+                       stats[pos].maximum,
+                       maxproc_counter[pos]);
+      (*out) << "| " << stats[pos].minimum << " " <<
+                        stats[pos].average << " " <<
+                        stats[pos].maximum << "\n";
+
+      // print description prefix again
+      (*out) << descriptions[pos]  << spacebuf << "|";
+
+      print_bar(*out, stats[pos].total, maxcounter[pos]);
+      // reset the space buffer
+      spacebuf[max_desc_length - descriptions[pos].length() + 1] =' ';
+
+       (*out) << "| " << stats[pos].total << " : " << maxcounter[pos] << " /s\n\n";
+    } while(hascounter.next_bit(pos));
+  }
+  if (found_events == false) {
+      ++noeventctr;
+  }
+  else {
+      noeventctr = 0;
+  }
+  hasevents = false;
+  (*out) << std::endl;
+}
+
+void dist_event_log::flush() {
+  if (rmi->procid() == 0) {
+    // move counters to the aggregator
+    uint32_t pos;
+    if (!hascounter.first_bit(pos)) return;
+    do {
+      size_t ctrval = counters[pos].exchange(0);
+      if (ctrval != 0) {
+        accumulate_event_aggregator(0, pos, ctrval);
+      }
+    } while(hascounter.next_bit(pos));
+    print_log();
+  }
+  else {
+    uint32_t pos;
+    if (!hascounter.first_bit(pos)) return;
+    do {
+      size_t ctrval = counters[pos].exchange(0);
+      if (ctrval != 0) {
+        rmi->remote_call(0,
+                         &dist_event_log::accumulate_event_aggregator,
+                         rmi->procid(),
+                         (unsigned char)pos, ctrval);
+      }
+    } while(hascounter.next_bit(pos));
+  }
+}
+
+void dist_event_log::thread_loop() {
+  m.lock();
+  while(!finished) {
+    if (flush_interval == 0) {
+      cond.wait(m);
+    }
+    else {
+      m.unlock();
+      my_sleep_ms(flush_interval);
+      m.lock();
+      if (flush_interval > 0) flush();
+    }
+  }
+  m.unlock();
+}
+
+} // namespace
--- src/graphlab/rpc/distributed_event_log.hpp
+++ src/graphlab/rpc/distributed_event_log.hpp
@@ -1,16 +1,17 @@
-#ifndef EVENT_LOG_HPP
-#define EVENT_LOG_HPP
+#ifndef DISTRIBUTED_EVENT_LOG_HPP
+#define DISTRIBUTED_EVENT_LOG_HPP
 #include <iostream>
 #include <boost/bind.hpp>
 #include <graphlab/parallel/pthread_tools.hpp>
 #include <graphlab/parallel/atomic.hpp>
 #include <graphlab/util/timer.hpp>
 #include <graphlab/util/dense_bitset.hpp>
+#include <graphlab/rpc/dc_dist_object.hpp>
 namespace graphlab {
 
 #define EVENT_MAX_COUNTERS 256
 
-class event_log{
+class dist_event_log{
  public:
   enum event_print_type{
     NUMBER,
@@ -27,10 +28,16 @@ class event_log{
   conditional cond;
   
   thread printing_thread;
+
+  // Local Counters
   std::string descriptions[EVENT_MAX_COUNTERS];
-  size_t maxcounter[EVENT_MAX_COUNTERS];
   atomic<size_t> counters[EVENT_MAX_COUNTERS];
-  
+
+  // Global counters on proc 0
+  std::vector<atomic<size_t> > globalcounters[EVENT_MAX_COUNTERS];
+  size_t maxcounter[EVENT_MAX_COUNTERS];
+  size_t maxproc_counter[EVENT_MAX_COUNTERS];  // maximum of per proc maximums
+
   timer ti;
   double prevtime;  // last time flush() was called
   bool hasevents;   // whether there are logging events
@@ -38,24 +45,29 @@ class event_log{
                      // zeroed when the events are next observed.
 
   size_t max_desc_length; // maximum descriptor length
-  fixed_dense_bitset<EVENT_MAX_COUNTERS> hascounter; 
+  fixed_dense_bitset<EVENT_MAX_COUNTERS> hascounter;
+
+  dc_dist_object<dist_event_log> *rmi;
  public:
-  inline event_log():out(NULL),
-                     flush_interval(0),
-                     print_method(DESCRIPTION),
-                     finished(false),
-                     prevtime(0),
-                     hasevents(false),
-                     noeventctr(0),
-                     max_desc_length(0) {
+  inline dist_event_log():out(NULL),
+                        flush_interval(0),
+                        print_method(DESCRIPTION),
+                        finished(false),
+                        prevtime(0),
+                        hasevents(false),
+                        noeventctr(0),
+                        max_desc_length(0),
+                        rmi(NULL) {
     hascounter.clear();
     for (size_t i = 0;i < EVENT_MAX_COUNTERS; ++i) {
       maxcounter[i] = 0;
+      maxproc_counter[i] = 0;
     }
-    printing_thread.launch(boost::bind(&event_log::thread_loop, this));
+    printing_thread.launch(boost::bind(&dist_event_log::thread_loop, this));
   }
   
-  void initialize(std::ostream &ostrm,
+  void initialize(distributed_control& dc,
+                  std::ostream &ostrm,
                   size_t flush_interval_ms,
                   event_print_type event_print);
 
@@ -65,20 +77,23 @@ class event_log{
 
   void add_event_type(unsigned char eventid, std::string description);
 
+  void accumulate_event_aggregator(procid_t proc,
+                                   unsigned char eventid,
+                                   size_t count);
+  
   inline void accumulate_event(unsigned char eventid,
                                size_t count)  __attribute__((always_inline)) {
-    hasevents = true;
     counters[eventid].inc(count);
   }
 
+  void print_log();
   void flush();
-
-  ~event_log();
+  ~dist_event_log();
 };
 
 }
 /**
- * DECLARE_EVENT_LOG(name)
+ * DECLARE_DIST_EVENT_LOG(name)
  * creates an event log with a given name. This creates a variable
  * called "name" which is of type event_log. and is equivalent to:
  *
@@ -89,40 +104,41 @@ class event_log{
  * to nothing when event logs are disabled.
  *
  *
- * INITIALIZE_EVENT_LOG(name, ostrm, flush_interval, printdesc)
+ * INITIALIZE_DIST_EVENT_LOG(name, dc, ostrm, flush_interval, printdesc)
  * ostrm is the output std::ostream object. A pointer of the stream
  * is taken so the stream should not be destroyed until the event log is closed
  * flush_interval is the flush frequency in milliseconds.
  * printdesc is either graphlab::event_log::NUMBER, or graphlab::event_log::DESCRIPTION or
- * graphlab::event_log::RATE_BAR
+ * graphlab::event_log::RATE_BAR. This must be called on all machines,
+ * but only machine 0 will use the ostrm, and printdesc arguments
  * 
- * ADD_EVENT_TYPE(name, id, desc)
+ * ADD_DIST_EVENT_TYPE(name, id, desc)
  * Creates an event type with an integer ID, and a description.
  * Event types should be mostly consecutive since the internal
  * storage format is a array. Valid ID range is [0, 255]
  *
- * ACCUMULATE_EVENT(name, id, count)
+ * ACCUMULATE_DIST_EVENT(name, id, count)
  * Adds 'count' events of type "id"
  *
- * FLUSH_EVENT_LOG(name)
+ * FLUSH_DIST_EVENT_LOG(name)
  * Forces a flush of the accumulated events to the provided output stream
  */
 #ifdef USE_EVENT_LOG
-#define DECLARE_EVENT_LOG(name) graphlab::event_log name;
-#define INITIALIZE_EVENT_LOG(name, ostrm, flush_interval, printdesc)    \
-                    name.initialize(ostrm, flush_interval, printdesc);
-#define ADD_EVENT_TYPE(name, id, desc) name.add_event_type(id, desc);
-#define ACCUMULATE_EVENT(name, id, count) name.accumulate_event(id, count);
-#define FLUSH_EVENT_LOG(name) name.flush();
-#define CLOSE_EVENT_LOG(name) name.close();
+#define DECLARE_DIST_EVENT_LOG(name) graphlab::dist_event_log name;
+#define INITIALIZE_DIST_EVENT_LOG(name, dc, ostrm, flush_interval, printdesc)    \
+                    name.initialize(dc, ostrm, flush_interval, printdesc);
+#define ADD_DIST_EVENT_TYPE(name, id, desc) name.add_event_type(id, desc);
+#define ACCUMULATE_DIST_EVENT(name, id, count) name.accumulate_event(id, count);
+#define FLUSH_DIST_EVENT_LOG(name) name.flush();
+#define CLOSE_DIST_EVENT_LOG(name) name.close();
 
 #else
-#define DECLARE_EVENT_LOG(name) 
-#define INITIALIZE_EVENT_LOG(name, ostrm, flush_interval, printdesc)
-#define ADD_EVENT_TYPE(name, id, desc) 
-#define ACCUMULATE_EVENT(name, id, count) 
-#define FLUSH_EVENT_LOG(name) 
-#define CLOSE_EVENT_LOG(name)
+#define DECLARE_DIST_EVENT_LOG(name) 
+#define INITIALIZE_DIST_EVENT_LOG(name, dc, ostrm, flush_interval, printdesc)
+#define ADD_DIST_EVENT_TYPE(name, id, desc) 
+#define ACCUMULATE_DIST_EVENT(name, id, count) 
+#define FLUSH_DIST_EVENT_LOG(name) 
+#define CLOSE_DIST_EVENT_LOG(name)
 #endif
 
 #endif
