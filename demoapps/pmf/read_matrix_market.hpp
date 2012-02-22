feb179bb6a9750b04140ab607e00ee138b7abf86
Danny Bickson <danny.bickson@gmail.com>
2012-02-21 20:10:09 -0500

    changed pmf output to dense matrix market format

--- demoapps/pmf/read_matrix_market.hpp
+++ demoapps/pmf/read_matrix_market.hpp
@@ -133,92 +133,141 @@ template<>
   assert(false);
 }
 
-void save_matrix_market_matrix(const char * filename, const mat & a){
+void save_matrix_market_matrix(const char * filename, const mat & a, std::string comment, bool integer, bool issparse){
     MM_typecode matcode;                        
     int i,j;
 
     mm_initialize_typecode(&matcode);
     mm_set_matrix(&matcode);
     mm_set_coordinate(&matcode);
-    mm_set_real(&matcode);
+    if (issparse)
+      mm_set_sparse(&matcode);
+    else mm_set_dense(&matcode);  
+
+    if (!integer)
+       mm_set_real(&matcode);
+    else
+       mm_set_integer(&matcode);
 
     FILE * f = open_file(filename,"w");
     assert(f != NULL);
     mm_write_banner(f, matcode); 
-    mm_write_mtx_crd_size(f, a.rows(), a.cols(), a.size());
+    if (comment.size() > 0)
+      fprintf(f, "%s%s", "%", comment.c_str());
 
-    for (i=0; i<a.rows(); i++)
-       for (j=0; j<a.cols(); j++)
-          if (get_val(a,i,j) != 0)
-               fprintf(f, "%d %d %10.3g\n", i+1, j+1, get_val(a,i,j));
-    
+    mm_write_mtx_crd_size(f, a.rows(), a.cols(), a.size());
+           
+
+    for (i=0; i<a.rows(); i++){
+       for (j=0; j<a.cols(); j++){
+          if (issparse){
+            if (get_val(a,i,j) != 0){
+               if (integer)
+                 fprintf(f, "%d %d %d\n", i+1, j+1, (int)get_val(a,i,j));
+               else  fprintf(f, "%d %d %10.13g\n", i+1, j+1, (double)get_val(a,i,j)); 
+            }
+          } else { //dense
+             if (integer)
+                fprintf(f, "%d ", (int)get_val(a,i,j));
+             else fprintf(f, "%10.13g ", (double)get_val(a,i,j));
+	     if (j == a.cols() -1 )
+                fprintf(f, "\n");
+          }
+       }
+    }
     logstream(LOG_INFO) << "Saved output matrix to file: " << filename << std::endl;
     logstream(LOG_INFO) << "You can read it with Matlab/Octave using the script mmread.m found on http://graphlab.org/mmread.m" << std::endl;
 
 }
 
-void save_matrix_market_vector(const char * filename, const vec & a){
+
+
+void save_matrix_market_vector(const char * filename, const vec & a, std::string comment, bool integer,bool issparse){
     MM_typecode matcode;                        
     int i;
 
     mm_initialize_typecode(&matcode);
     mm_set_matrix(&matcode);
-    //mm_set_coordinate(&matcode);
-    mm_set_array(&matcode);
-    mm_set_real(&matcode);
+    if (issparse){
+      mm_set_sparse(&matcode);
+      mm_set_coordinate(&matcode);
+    }
+    else {
+      mm_set_dense(&matcode);
+      mm_set_array(&matcode);
+    }
+    if (!integer)
+      mm_set_real(&matcode);
+    else
+      mm_set_integer(&matcode);
 
     FILE * f = open_file(filename,"w");
     mm_write_banner(f, matcode); 
-    mm_write_mtx_array_size(f, a.size(), 1);
+    if (comment.size() > 0)
+      fprintf(f, "%s%s", "%", comment.c_str());
+    
+    if (issparse)
+      mm_write_mtx_crd_size(f, a.size(), 1, a.size());
+    else 
+      mm_write_mtx_array_size(f, a.size(), 1);
 
-    for (i=0; i<a.size(); i++)
-        fprintf(f, "%10.3g\n", a[i]);
+    for (i=0; i<a.size(); i++){
+      if (issparse){
+        if (integer)
+          fprintf(f, "%d %d %d\n", i+1, 1, (int)a[i]);
+        else fprintf(f, "%d %d %10.13g\n", i+1, 1, a[i]);
+      }
+      else {//dense
+        if (integer)
+          fprintf(f,"%d ", (int)a[i]);
+        else fprintf(f, "%10.13g\n", a[i]);
+      }
+    }
 
     logstream(LOG_INFO) << "Saved output vector to file: " << filename << std::endl;
     logstream(LOG_INFO) << "You can read it with Matlab/Octave using the script mmread.m found on http://graphlab.org/mmread.m" << std::endl;
 }
 
 
-
 void save_matrix_market_format(const char * filename, mat &U, mat& V)
 {
     if (ps.algorithm != SVD && ps.algorithm != SVD_PLUS_PLUS && ps.algorithm != TIME_SVD_PLUS_PLUS){
-      save_matrix_market_matrix((std::string(filename) + ".V").c_str(),V);
-      save_matrix_market_matrix((std::string(filename) + ".U").c_str(),U);
+      save_matrix_market_matrix((std::string(filename) + ".V").c_str(),V, "%%GraphLab Collaborative filtering library. This file holds the matrix V. Row i holds the feature vector for movie i. You can compute prediction in matlab for user i movie j using U(i,:)*V(j,:)'\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".U").c_str(),U, "%%GraphLab Collaborative filtering library. This file holds the matrix U. Row i holds the feature vector for user i. You can compute prediction in matlab for user i movie j using U(i,:)*V(j,:)'\n", false, false);
       return;
     }
     
     if (ps.algorithm == SVD){
-      save_matrix_market_matrix((std::string(filename) + ".V").c_str(),U); /* for conforming to wikipedia convention, I swap U and V*/
-      save_matrix_market_matrix((std::string(filename) + ".U").c_str(),V);
-      save_matrix_market_vector((std::string(filename) + ".EigenValues_AAT").c_str(),get_col(ps.T,0));
-      save_matrix_market_vector((std::string(filename) + ".EigenValues_ATA").c_str(),get_col(ps.T,1));
+      save_matrix_market_matrix((std::string(filename) + ".V").c_str(),U, "%%GraphLab collaborative filtering library. This file holds the matrix V which is the output of SVD\n", false, false); /* for conforming to wikipedia convention, I swap U and V*/
+      save_matrix_market_matrix((std::string(filename) + ".U").c_str(),V, "%%GraphLab collaborative filtering library. This file holds the matrix U which is the output of SVD\n", false, false);
+      save_matrix_market_vector((std::string(filename) + ".EigenValues_AAT").c_str(),get_col(ps.T,0), "%%GraphLab collaborative filtering library. This file holds eigenvalues of the matrix A*A'\n", false, false);
+      save_matrix_market_vector((std::string(filename) + ".EigenValues_ATA").c_str(),get_col(ps.T,1), "%%GraphLab collaborative filtering library. This file holds eigenvalues of the matrix A'*A\n", false, false);
       return;
     }
 
     if (ps.algorithm == SVD_PLUS_PLUS){
-      save_matrix_market_vector((std::string(filename) + ".UserBias").c_str(),ps.svdpp_usr_bias);
-      save_matrix_market_vector((std::string(filename) + ".MovieBias").c_str(),ps.svdpp_movie_bias);
-      save_matrix_market_matrix((std::string(filename) + ".Users").c_str(),ps.U);
-      save_matrix_market_matrix((std::string(filename) + ".Movies").c_str(),ps.V);
+      save_matrix_market_vector((std::string(filename) + ".UserBias").c_str(),ps.svdpp_usr_bias, "%%GraphLab collaborative filtering library. This file holds user bias vector. In row i we have bias of user i.\n", false, false);
+      save_matrix_market_vector((std::string(filename) + ".MovieBias").c_str(),ps.svdpp_movie_bias, "%%GraphLab collaborative filtering library. This file holds user bias vector. In row i we have bias of movie i.\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Users").c_str(),ps.U,"%%GraphLab Collaborative filtering library. This file holds the matrix U. Row i holds the feature vector for user i.\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Movies").c_str(),ps.V,"%%GraphLab Collaborative filtering library. This file holds the matrix V. Row i holds the feature vector for movie i.\n", false, false);
       mat gmean = mat(1,1);
       set_val(gmean,0,0,ps.globalMean[0]);
-      save_matrix_market_matrix((std::string(filename) + ".GlobalMean").c_str(),gmean);
+      save_matrix_market_matrix((std::string(filename) + ".GlobalMean").c_str(),gmean, "%%GraphLab collaborative filtering library. This file holds the global mean value.\n", false, false);
       return;
     }
 
     if (ps.algorithm == TIME_SVD_PLUS_PLUS){
-      save_matrix_market_vector((std::string(filename) + ".UserBias").c_str(),ps.svdpp_usr_bias);
-      save_matrix_market_vector((std::string(filename) + ".MovieBias").c_str(),ps.svdpp_movie_bias);
-      save_matrix_market_matrix((std::string(filename) + ".Users_ptemp").c_str(),ps.timesvdpp_out.ptemp);
-      save_matrix_market_matrix((std::string(filename) + ".Users_x").c_str(),ps.timesvdpp_out.x);
-      save_matrix_market_matrix((std::string(filename) + ".Users_pu").c_str(),ps.timesvdpp_out.pu);
-      save_matrix_market_matrix((std::string(filename) + ".Movies_q").c_str(),ps.timesvdpp_out.q);
-      save_matrix_market_matrix((std::string(filename) + ".Time_z").c_str(),ps.timesvdpp_out.z);
-      save_matrix_market_matrix((std::string(filename) + ".Time_pt").c_str(),ps.timesvdpp_out.pt);
+      save_matrix_market_vector((std::string(filename) + ".UserBias").c_str(),ps.svdpp_usr_bias,"%%GraphLab collaborative filtering library. This file holds user bias vector. In row i we have bias of user i.\n", false, false);
+      save_matrix_market_vector((std::string(filename) + ".MovieBias").c_str(),ps.svdpp_movie_bias,"%%GraphLab collaborative filtering library. This file holds user bias vector. In row i we have bias of movie i.\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Users_ptemp").c_str(),ps.timesvdpp_out.ptemp,"%%GraphLab collaborative filtering library. This file holds ptemp array for time-SVD++\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Users_x").c_str(),ps.timesvdpp_out.x,"%%GraphLab collaborative filtering library. This file holds x array for time-SVD++\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Users_pu").c_str(),ps.timesvdpp_out.pu,"%%GraphLab collaborative filtering library. This file holds pu array for time-SVD++\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Movies_q").c_str(),ps.timesvdpp_out.q,"%%GraphLab collaborative filtering library. This file holds q array for time-SVD++\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Time_z").c_str(),ps.timesvdpp_out.z,"%%GraphLab collaborative filtering library. This file holds z array for time-SVD++\n", false, false);
+      save_matrix_market_matrix((std::string(filename) + ".Time_pt").c_str(),ps.timesvdpp_out.pt,"%%GraphLab collaborative filtering library. This file holds pt array for time-SVD++\n", false, false);
       mat gmean = mat(1,1);
       set_val(gmean,0,0,ps.globalMean[0]);
-      save_matrix_market_matrix((std::string(filename) + ".GlobalMean").c_str(),gmean);
+      save_matrix_market_matrix((std::string(filename) + ".GlobalMean").c_str(),gmean,"%%GraphLab collaborative filtering library. This file holds the global mean value.\n",false,false);
       return;
     }
 }
