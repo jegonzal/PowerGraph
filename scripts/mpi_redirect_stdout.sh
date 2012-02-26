1856a305efe114678d5f17e34294130444c6c145
Yucheng Low <yuchenglow@gmail.com>
2012-02-26 16:11:46 -0500

    Added mpi stdou redirection script

---
+++ scripts/mpi_redirect_stdout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -z "$PMI_RANK" ]; then
+RANK=$PMI_RANK
+elif [ ! -z "$OMPI_COMM_WORLD_RANK" ]; then
+RANK=$OMPI_COMM_WORLD_RANK
+else
+echo "Unable to figure out MPI Rank!"
+exit 1
+fi
+#echo $RANK
+$* | tee out.$RANK 
