1b07508b19a97c5e72fcd3c4901fc2157db1a9b7
Danny Bickson <danny.bickson@gmail.com>
2012-03-13 07:32:19 -0400

    finished working on parsing matrix market matrices with more than 2billion edges

--- toolkits/parsers/convert_mm_to_reverse.m
+++ toolkits/parsers/convert_mm_to_reverse.m
@@ -1,7 +1,5 @@
 #!/bin/sh
-#script for converting matrix market format to its tranpose matrix
-#written by danny bickson CMU
 rm -f $1.reverse
 head -n 1 $1 > $1.reverse
-head -n 3 $1 | grep -v "%" | head -n 1 | awk '{print $2 " " $1 " " $3 }' >> $1.reverse
-cat $1 | grep -v "%" | awk '{print $2 " " $1 " " $3}'| sort -n -k 2,2 -k 1,1 -T . >> $1.reverse
+head -n 3 $1 | grep -v "%" | head -n 1 | awk '{print $1 " " $2 " " $3 }' >> $1.reverse
+cat $1 | grep -v "%" | sort -n -k 2,2 -k 1,1 -T . >> $1
