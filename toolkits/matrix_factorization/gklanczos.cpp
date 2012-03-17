9dade9a25a15bd29134f87b12354dd1978f73c84
Danny Bickson <danny.bickson@gmail.com>
2012-03-17 16:59:41 -0400

    debugging brian memory bug on thrust

--- toolkits/matrix_factorization/gklanczos.cpp
+++ toolkits/matrix_factorization/gklanczos.cpp
@@ -97,10 +97,14 @@ void init_lanczos(graph_type * g, bipartite_graph_descriptor & info){
      logstream(LOG_FATAL)<<"Failed to load graph. Aborting" << std::endl;
 
   data_size = nsv + nv+1 + max_iter;
+  int actual_vector_len = data_size;
+  if (info.is_square())
+     actual_vector_len = 2*data_size;
 #pragma omp parallel for
   for (int i=0; i< info.total(); i++)
-      g->vertex_data(i).pvec = zeros(info.is_square() ? (2*data_size): data_size);
-  logstream(LOG_INFO)<<"Allocated a total of: " << ((double)data_size * g->num_vertices()/ 1e6) << " MB for storing vectors." << std::endl;
+      g->vertex_data(i).pvec = zeros(actual_vector_len);
+
+  logstream(LOG_INFO)<<"Allocated a total of: " << ((double)actual_vector_len * g->num_vertices() * sizeof(double)/ 1e6) << " MB for storing vectors." << std::endl;
 }
 
 
@@ -112,7 +116,7 @@ vec lanczos(graphlab::core<graph_type, Axb> & glcore, bipartite_graph_descriptor
    int nconv = 0;
    int its = 1;
    int mpd = 24;
-   int N = std::min(info.rows, info.cols);
+   //int N = std::min(info.rows, info.cols);
    DistMat A(info);
    DistSlicedMat U(info.is_square() ? data_size : 0, info.is_square() ? 2*data_size : data_size, true, info, "U");
    DistSlicedMat V(0, data_size, false, info, "V");
