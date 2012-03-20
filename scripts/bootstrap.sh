9710b9efda7df08286cfe95a6c1b182064e79df5
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2012-03-20 10:15:58 -0400

    Completely removed kyoto cabinent dependency.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,18 +14,3 @@ if [ ! -z $BOOST_ROOT ] && [ $BOOST_ROOT == $PWD/$DEPS_DIR ]; then
   echo "BOOST_ROOT=$BOOST_ROOT" >> configure.deps
 fi
 
-source scripts/kccheck.sh
-# we add a boost root only if it is the deps directory
-if [ ! -z $KC_ROOT ] && [ $KC_ROOT == $PWD/$DEPS_DIR ]; then
-  echo "KC_ROOT=$KC_ROOT" >> configure.deps
-fi
-
-
-# if [ ! -z $YRL_EXPERIMENTAL ]; then
-#   echo "Installing AVRO"
-#   source scripts/avrocheck.sh
-#   if [ ! -z $AVRO_ROOT ] && [ $AVRO_ROOT == $PWD/$DEPS_DIR ]; then
-#     echo "AVRO_ROOT=$AVRO_ROOT" >> configure.deps
-#   fi
-# fi
-
