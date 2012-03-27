07d03e8283082cd8a5dc8b88a81d4421ecabf2a1
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2012-03-27 02:33:54 -0400

    removed unnecessary configure commands

--- scripts/install_graphlabpi_v2.sh
+++ scripts/install_graphlabpi_v2.sh
@@ -3,12 +3,7 @@
 hg clone https://code.google.com/p/graphlabapi/ 
 cd graphlabapi
 hg up v2
-./configure --bootstrap \
-    --force_cmake_install \
-    --force_boost_install \
-    --eigen \
-    --tcmalloc --force_tcmalloc_install \
-    --yes | tee v2_install_configure_log.txt
+./configure | tee v2_install_configure_log.txt
 cd debug
 make -j2 | tee ../v2_debug_log.txt
 
