a4ca8b61c67034dbef9c84520fb541813957f34d
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2012-04-05 19:54:11 -0400

    fixed powerlaw generation bug

--- toolkits/synthetic_data/make_datasets.sh
+++ toolkits/synthetic_data/make_datasets.sh
@@ -4,8 +4,8 @@ create_powerlaw_graph=$PWD/create_powerlaw_graph
 convert_graph=$PWD/convert_graph
 
 nverts=10000000
-fanout=10
-alphas=$(seq 1 .2 3)
+
+alphas=$(seq 1.8 .2 3)
 
 
 mkdir datasets
@@ -18,15 +18,15 @@ function make_graphs {
     mkdir $dirname
     cd $dirname
     echo "Making graph"
-    graphname=nverts_${nverts}_fanout_${fanout}_alpha_${alpha}
+    graphname=nverts_${nverts}_fanout_alpha_${alpha}
     echo $graphname
-    $create_powerlaw_graph --nverts=$nverts --fanout=$fanout --alpha=$alpha \
+    $create_powerlaw_graph --nverts=$nverts --alpha=$alpha \
         --graph ${graphname}.tsv
     echo "Constructing adj"
     $convert_graph ${graphname}.tsv edge2adj ${graphname}.adj
     
     echo "Reversing graph"
-    rev_graphname=nverts_${nverts}_fanin_${fanout}_alpha_${alpha}
+    rev_graphname=nverts_${nverts}_fanin_alpha_${alpha}
     echo $graphname   
     $convert_graph ${graphname}.tsv reverse ${rev_graphname}.tsv
     echo "constructing adj"
