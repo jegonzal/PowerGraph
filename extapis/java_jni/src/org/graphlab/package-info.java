2c7a56c7550e8c2f705a810bd270a366fac1b928
Jim Lim <jiunnhal@cmu.edu>
2012-04-06 23:21:52 -0400

    ant JAVA_HOME not working; reverted.

--- extapis/java_jni/src/org/graphlab/package-info.java
+++ extapis/java_jni/src/org/graphlab/package-info.java
@@ -1,6 +1,16 @@
 /**
- * Provides basic GraphLab classes. More utilities may be found in {@link org.graphlab.data}
+ * Provides basic GraphLab classes.
+ * <p>More utilities may be found in {@link org.graphlab.data}
  * and {@link org.graphlab.toolkits}, but this package has the minimum classes you need to
- * get started.
+ * get started.</p>
+ *
+ * <h3>Installation</h3>
+ * <p>First, obtain a copy of GraphLab from <a href='http://graphlab.org/download.html'>the official site</a>.
+ * Ensure that you have Java 1.6 and above before running the compilation script. The script will compile
+ * the GraphLab shared library as well as the JNI interface.</p>
+ * 
+ * <h3>Making an App</h3>
+ * <p>To create a Java app, execute <code>make java_app</code> while you are in the <code>graphlabapi/release</code>
+ * directory. An Eclipse project with all the necessary libraries will be created for you at </p>
  */
 package org.graphlab;
