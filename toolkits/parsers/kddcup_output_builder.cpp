d9b3413749efa2389bc171ecbcc37ec449712531
Danny Bickson <danny.bickson@gmail.com>
2012-04-12 08:43:15 -0400

    cleaned kdd cup code

--- toolkits/parsers/kddcup_output_builder.cpp
+++ toolkits/parsers/kddcup_output_builder.cpp
@@ -16,7 +16,9 @@
  *
  * For more about this software visit:
  *
- *      http://www.graphlab.ml.cmu.edu
+ *      http://graphlab.org
+ *
+ * Code by Danny Bickson, CMU
  *
  */
 
@@ -24,44 +26,24 @@
 #include <cstdio>
 #include <map>
 #include <iostream>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/filtering_stream.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-#include <boost/unordered_map.hpp>
-#include <boost/archive/text_oarchive.hpp>
-#include <graphlab/serialization/oarchive.hpp>
-#include <graphlab/serialization/iarchive.hpp>
-#include <graphlab/serialization/unordered_map.hpp>
-#define AVOID_PARALLEL_SORT 1
-#define GRAPH2_NO_EDGE
-#include <graphlab/graph/graph3.hpp>
 #include "graphlab.hpp"
 #include "../shared/io.hpp"
 #include "../shared/types.hpp"
-#include <graphlab/macros_def.hpp>
 using namespace graphlab;
 using namespace std;
 
-bool debug = false;
-bool quick = false;
-boost::unordered_map<string,uint> hash2nodeid;
 string datafile;
 string prediction_file;
-uint conseq_id;
-graphlab::mutex mymutex;
-graphlab::timer mytime;
-unsigned long long total_lines = 0;
-unsigned long long self_edges = 0;
+bool debug = false;
 
 struct vertex_data {
-  string filename;
-  string prediction_file; 
+  string filename; //file name with test data (either earlier or later)
+  string prediction_file;  //file name with computed predictions for each test item
   vertex_data() { }
   vertex_data(string _filename, string _prediction_file) : filename(_filename), prediction_file(_prediction_file) { }
 }; // end of vertex_data
 
 struct edge_data {
-  double val;
 };
 
 typedef graphlab::graph<vertex_data, edge_data> graph_type;
@@ -91,8 +73,8 @@ struct edge_data2 {
 
 typedef graphlab::graph<vertex_data2, edge_data2> graph_type2;
 
-struct stringzipparser_update :
-   public graphlab::iupdate_functor<graph_type, stringzipparser_update>{
+struct kdd_output_builder :
+   public graphlab::iupdate_functor<graph_type, kdd_output_builder>{
    void operator()(icontext_type& context) {
    
 typedef graphlab::graph<vertex_data2, edge_data2>::edge_list_type edge_list;
@@ -115,21 +97,27 @@ typedef graphlab::graph<vertex_data2, edge_data2>::edge_list_type edge_list;
  
     int cnt = 0;
     int users = 0;
+    //go over each user in test data
     for (int i=0; i< nodes; i++){
       vec ratings = zeros(_graph.num_out_edges(i));
       edge_list edges  = _graph.out_edges(i);
+      //find out the ratings computed by graphlab
       for (uint j=0; j< edges.size(); j++){
          ratings[j] = ret[cnt]; 
          cnt++;
       }
       if (edges.size() > 0 ){
         users++;
+        //sort the rating
         ivec pos = sort_index(ratings);
+        //print user id
         fprintf(pfile, "%d,", i+1);
+        //print out the 3 items with the highest ratings (or only 2 if there are only two items)
         for (int j=0; j< std::min((int)ratings.size(),3); j++){
           fprintf(pfile, "%d ", edges[pos[ratings.size() - j - 1]].target()-nodes+1);
         }
         fprintf(pfile, "\n");
+
         if (i ==100019){ //sanity check to verify this user has the right item (there are only 2 items for her)
            assert((edges[pos[0]].target() -nodes+1)==1606574 || (edges[pos[1]].target() -nodes+1)==1606574);
            assert((edges[pos[0]].target() -nodes+1)==1774684 || (edges[pos[1]].target() -nodes+1)==1774684);
@@ -159,7 +147,6 @@ int main(int argc,  char *argv[]) {
   clopts.add_positional("data");
   clopts.attach_option("prediction_file", &prediction_file, prediction_file, "prediction file name");
   clopts.add_positional("prediction_file");
-  clopts.attach_option("debug", &debug, debug, "Display debug output.");
   clopts.attach_option("num_nodes", &numnodes, numnodes, "Number of nodes");
 
   // Parse the command line arguments
@@ -171,32 +158,22 @@ int main(int argc,  char *argv[]) {
   logstream(LOG_WARNING)
     << "Eigen detected. (This is actually good news!)" << std::endl;
   logstream(LOG_INFO) 
-    << "GraphLab Linear solver library code by Danny Bickson, CMU" 
+    << "GraphLab Parsers library code by Danny Bickson, CMU" 
     << std::endl 
     << "Send comments and bug reports to danny.bickson@gmail.com" 
-    << std::endl 
-    << "Currently implemented algorithms are: Gaussian Belief Propagation, "
-    << "Jacobi method, Conjugate Gradient" << std::endl;
-
-
-
+    << std::endl;
+ 
   // Create a core
-  graphlab::core<graph_type, stringzipparser_update> core;
+  graphlab::core<graph_type, kdd_output_builder> core;
   core.set_options(clopts); // Set the engine options
-  core.set_scope_type("vertex");
-  mytime.start();
 
   vertex_data data(datafile, prediction_file);
   core.graph().add_vertex(0, data);
 
   std::cout << "Schedule all vertices" << std::endl;
-  core.schedule_all(stringzipparser_update());
+  core.schedule_all(kdd_output_builder());
  
-  //accumulator acum;
-  //core.add_sync("sync", acum, sync_interval);
   core.add_global("NUM_NODES", numnodes);
-
-
   double runtime= core.start();
  
   std::cout << "Finished in " << runtime << std::endl;
@@ -204,5 +181,4 @@ int main(int argc,  char *argv[]) {
 }
 
 
-#include <graphlab/macros_undef.hpp>
 
