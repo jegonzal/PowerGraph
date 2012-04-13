ac021f9b3248356741feece1c828f2b5ff1d0b9d
Danny Bickson <danny.bickson@gmail.com>
2012-04-13 17:49:27 -0400

    changed field name in command line option because of possible parsing bug

--- demoapps/pmf/advanced_config.cpp
+++ demoapps/pmf/advanced_config.cpp
@@ -24,7 +24,7 @@ void advanced_config::init_command_line_options(graphlab::command_line_options &
   clopts.attach_option("binaryoutput", &binaryoutput, binaryoutput, "export U,V,T to a binary file"); 
   clopts.attach_option("exporttest", &exporttest, exporttest, "export predictions on test data to file");
   clopts.attach_option("matrixmarket", &matrixmarket, matrixmarket, "give input in matrix market format"); 
-  clopts.attach_option("matrixmarkettokensperrow", &matrixmarkettokensperrow, matrixmarkettokensperrow, "Number of matrix market token per row. Default is 3: [from ] [to ] [ val ] ");
+  clopts.attach_option("matrix_market_tokens_per_row", &matrixmarkettokensperrow, matrixmarkettokensperrow, "Number of matrix market token per row. Default is 3: [from ] [to ] [ val ] ");
  
   //BPTF related switches
   clopts.attach_option("bptf_alpha", &bptf_alpha, bptf_alpha, "BPTF alpha (noise parameter)");  
