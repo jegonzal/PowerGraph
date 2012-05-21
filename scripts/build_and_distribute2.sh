a6ec07bdee728266252e219081495faf84929ba6
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2012-05-21 15:35:58 -0400

    more cleaning and restructuring

--- scripts/build_and_distribute2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /mnt/graphlabapi/release/apps/dist2_apps/webgraph_bp
-make -j8 webgraph_bp
-cp /mnt/graphlabapi/release/apps/dist2_apps/webgraph_bp/webgraph_bp /mnt/bin
-cd /mnt
-mpiexec -n 64 -f ~/hosts -wdir /mnt /mnt/bin/dirdist -s bin -f 3
-mpiexec -n 64 -f ~/hosts chmod +x /mnt/bin/*
-
-
