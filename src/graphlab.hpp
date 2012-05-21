168aefdc6d3b7316a56b94af275cac4dbde6ca3c
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2012-05-21 15:38:49 -0400

    remving distributed_graphlab.hpp in place of graphlab.hpp

--- src/graphlab.hpp
+++ src/graphlab.hpp
@@ -44,6 +44,17 @@
 #include <graphlab/core.hpp>
 
 
+#include <graphlab.hpp>
+
+#include <graphlab/util/mpi_tools.hpp>
+
+#include <graphlab/rpc/dc.hpp>
+#include <graphlab/rpc/dc_init_from_mpi.hpp>
+#include <graphlab/engine/distributed_engine2.hpp>
+#include <graphlab/engine/distributed_fscope_engine.hpp>
+#include <graphlab/engine/distributed_synchronous_engine.hpp>
+#include <graphlab/graph/distributed_graph.hpp>
+
 
 
 
