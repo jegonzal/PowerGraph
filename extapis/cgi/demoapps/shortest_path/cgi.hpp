c0ca2b7b355a226ff38eefbd053e9aba07b918da
Jim Lim <jiunnhal@cmu.edu>
2012-06-09 23:15:01 -0700

    refactored code to create cgi client class;

---
+++ extapis/cgi/demoapps/shortest_path/cgi.hpp
@@ -0,0 +1,166 @@
+/**  
+ * Copyright (c) 2009 Carnegie Mellon University. 
+ *     All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS
+ *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied.  See the License for the specific language
+ *  governing permissions and limitations under the License.
+ *
+ * For more about this software visit:
+ *
+ *      http://www.graphlab.ml.cmu.edu
+ *
+ */
+ 
+/**
+ * @file cgi.hpp
+ * Convenience methods for writing a C++ GraphLab CGI program. You don't really
+ * need this if you are using your own json parser (i.e. not using rapidjson)
+ * @author Jiunn Haur Lim <jiunnhal@cmu.edu>
+ */
+
+#ifndef GRAPHLAB_CGI_HPP
+#define GRAPHLAB_CGI_HPP
+
+#include <sstream>
+#include <iostream>
+#include "../../rapidjson.hpp"
+
+/** initial read buffer size */
+#define INITIAL_LENGTH 256
+
+namespace graphlab {
+
+  /**
+   * Interface for all CGI handlers. Every method takes a JSON document and returns
+   * a JSON document.
+   */
+  class icgi_handler {
+    public:
+      virtual ~icgi_handler() {}
+      virtual void init(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+      virtual void gather_edges(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+      virtual void scatter_edges(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+      virtual void gather(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+      virtual void merge(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+      virtual void apply(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+      virtual void scatter(rapidjson::Document& invocation, rapidjson::Document& return_json) = 0;
+  };
+  
+  class cgi {
+  private:
+    icgi_handler& handler;
+    
+  public:
+    
+    /**
+     * Creates a new CGI client.
+     * @param[in]   cgi_handler     implements icgi_handler and contains most of
+     *                              the program logic
+     */
+    cgi(icgi_handler& cgi_handler) : handler(cgi_handler){}
+    
+    void listen(){
+      
+      std::string line;
+      std::size_t length = 0;
+      std::size_t current_length = INITIAL_LENGTH;
+      char *buffer = new char[current_length];
+  
+      // loop until exit is received
+      while (true){
+    
+        // TODO: assume NULL-terminated string for now
+        
+        // read length, and expand buffer if necessary
+        std::cin >> length; std::getline(std::cin, line);
+        if (length + 1 > current_length){
+          current_length = length + 1;
+          delete[] buffer;
+          buffer = new char[current_length];
+        }
+    
+        // read message
+        std::cin.read(buffer, length);
+        buffer[length] = '\0';    // terminate string w. null
+        rapidjson::StringBuffer return_buffer;
+    
+        // invoke corresponding callback method
+        std::cerr << buffer << std::endl;
+        const char *return_json = handle_invocation(buffer, return_buffer);
+        if (!return_json) break;
+    
+        // return
+        std::cout << strlen(return_json) << "\n";
+        std::cout << return_json << std::flush;
+    
+      }
+  
+      delete[] buffer;
+      
+    }
+    
+  private:
+  
+    const char *handle_invocation(const char *buffer, rapidjson::StringBuffer& return_buffer){
+
+      if (NULL == buffer) return NULL;
+    
+      rapidjson::Document invocation;
+      if (invocation.Parse<0>(buffer).HasParseError()){
+        std::ostringstream error("JSON syntax error: ");
+        error << buffer;
+        throw error.str();
+      }
+      
+      if (!invocation.HasMember("method"))
+        throw "Missing method field.";
+
+      if (!strcmp(invocation["method"].GetString(), "exit"))
+        return NULL;
+      
+      rapidjson::Document return_json;
+      return_json.SetObject();
+      
+      // invoke appropriate method
+      if (!strcmp(invocation["method"].GetString(), "init"))
+        handler.init(invocation, return_json);
+      else if (!strcmp(invocation["method"].GetString(), "gather_edges"))
+        handler.gather_edges(invocation, return_json);
+      else if (!strcmp(invocation["method"].GetString(), "scatter_edges"))
+        handler.scatter_edges(invocation, return_json);
+      else if (!strcmp(invocation["method"].GetString(), "gather"))
+        handler.gather(invocation, return_json);
+      else if (!strcmp(invocation["method"].GetString(), "merge"))
+        handler.merge(invocation, return_json);
+      else if (!strcmp(invocation["method"].GetString(), "apply"))
+        handler.apply(invocation, return_json);
+      else if (!strcmp(invocation["method"].GetString(), "scatter"))
+        handler.scatter(invocation, return_json);
+      else {
+        std::ostringstream error("Unknown method: ");
+        error << invocation["method"].GetString();
+        throw error.str();
+      }
+      
+      // write return json to stdout
+      return_buffer.Clear();
+      rapidjson::Writer<rapidjson::StringBuffer> writer(return_buffer);
+      return_json.Accept(writer);
+      return return_buffer.GetString();
+      
+    }
+    
+  };
+  
+};
+
+#endif
--- extapis/cgi/demoapps/shortest_path/shortest_path.cpp
+++ extapis/cgi/demoapps/shortest_path/shortest_path.cpp
@@ -1,172 +1,111 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <iostream>
-#include <sstream>
 
-#include "../../rapidjson.hpp"
+#include "cgi.hpp"
 
-#define INITIAL_LENGTH 256
 #define INFINITY       1e+99
 
+using namespace graphlab;
 namespace json = rapidjson;
 
-double stof(const char *str){
+/**
+ * Converts cstring to double. If cstring is empty, return INFINITY.
+ */
+static double stof(const char *str){
   if (0 == strlen(str)) return INFINITY;
   return atof(str);
 }
 
-void init(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  return_json.AddMember("program", "x", allocator);
-}
+/**
+ * CGI Handler - contains methods (callbacks) to handle JSON invocations. Refer
+ * to main GraphLab documentation for description of these methods.
+ */
+class cgi_handler : public icgi_handler {
+public:
 
-void gather_edges(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  return_json.AddMember("edges", "IN_EDGES", allocator);
-}
-
-void scatter_edges(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  return_json.AddMember("edges", "OUT_EDGES", allocator);
-}
-
-void gather(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  // find distance from source to this vertex thru neighbor
-  const json::Value& edge = invocation["params"]["edge"];
-  double edge_dist = atof(edge["state"].GetString());
-  double nbr_dist = stof(edge["source"]["state"].GetString());
-  double vertex_dist = nbr_dist + edge_dist;
-  // return gather result
-  std::ostringstream double_stream;
-  double_stream << vertex_dist;
-  const char *double_str = double_stream.str().c_str();
-  json::Value dist(double_str, allocator);
-  return_json.AddMember("result", dist, allocator);
-}
-
-void merge(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  // keep the shortest distance
-  double left = stof(invocation["state"].GetString());
-  double right = stof(invocation["params"]["other"].GetString());
-  double min_dist = std::min(left, right);
-  // return merged gather result
-  std::ostringstream double_stream;
-  double_stream << min_dist;
-  const char *double_str = double_stream.str().c_str();
-  json::Value dist(double_str, allocator);
-  return_json.AddMember("result", dist, allocator);
-}
-
-void apply(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  // current vertex distance
-  const char *vertex_state = invocation["params"]["vertex"]["state"].GetString();
-  double vertex_dist = stof(vertex_state);
-  // best distance found from gather
-  double best_dist = stof(invocation["params"]["gather"].GetString());
-  // if improvement, update
-  if (best_dist < vertex_dist){
+  void init(json::Document& invocation, json::Document& return_json){
+    // reset state of vertex program
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    return_json.AddMember("program", "", allocator);
+  }
+  
+  void gather_edges(json::Document& invocation, json::Document& return_json){
+    // we want to gather all incoming edges
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    return_json.AddMember("edges", "IN_EDGES", allocator);
+  }
+  
+  void scatter_edges(json::Document& invocation, json::Document& return_json){
+    // scatter to all outgoing edges
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    return_json.AddMember("edges", "OUT_EDGES", allocator);
+  }
+  
+  void gather(json::Document& invocation, json::Document& return_json){
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    // find distance from source to this vertex thru neighbor
+    const json::Value& edge = invocation["params"]["edge"];
+    double edge_dist = atof(edge["state"].GetString());
+    double nbr_dist = stof(edge["source"]["state"].GetString());
+    double vertex_dist = nbr_dist + edge_dist;
+    // return gather result
     std::ostringstream double_stream;
-    double_stream << best_dist;
+    double_stream << vertex_dist;
     const char *double_str = double_stream.str().c_str();
     json::Value dist(double_str, allocator);
-    return_json.AddMember("vertex", dist, allocator);
+    return_json.AddMember("result", dist, allocator);
   }
-}
-
-void scatter(json::Document& invocation, json::Document& return_json){
-  json::Document::AllocatorType& allocator = return_json.GetAllocator();
-  // best distance found for this vertex
-  double best_dist = stof(invocation["params"]["vertex"]["state"].GetString());
-  // parse distances
-  const json::Value& edge = invocation["params"]["edge"];
-  double nbr_dist = stof(edge["target"]["state"].GetString());
-  double edge_dist = atof(edge["state"].GetString());
-  // relax edge
-  if (best_dist < INFINITY && nbr_dist > (best_dist + edge_dist)){
-    return_json.AddMember("signal", "TARGET", allocator);
+  
+  void merge(json::Document& invocation, json::Document& return_json){
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    // keep the shortest distance
+    double left = stof(invocation["state"].GetString());
+    double right = stof(invocation["params"]["other"].GetString());
+    double min_dist = std::min(left, right);
+    // return merged gather result
+    std::ostringstream double_stream;
+    double_stream << min_dist;
+    const char *double_str = double_stream.str().c_str();
+    json::Value dist(double_str, allocator);
+    return_json.AddMember("result", dist, allocator);
   }
-}
-
-const char *handle_invocation(const char *buffer, json::StringBuffer& return_buffer){
-
-  if (NULL == buffer) return NULL;
-
-  json::Document invocation;
-  if (invocation.Parse<0>(buffer).HasParseError()){/* TODO: error handling */}
   
-  // TODO: error handling for missing elements
-  if (!strcmp(invocation["method"].GetString(), "exit")) return NULL;
+  void apply(json::Document& invocation, json::Document& return_json){
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    // current vertex distance
+    const char *vertex_state = invocation["params"]["vertex"]["state"].GetString();
+    double vertex_dist = stof(vertex_state);
+    // best distance found from gather
+    double best_dist = stof(invocation["params"]["gather"].GetString());
+    // if improvement, update
+    if (best_dist < vertex_dist){
+      std::ostringstream double_stream;
+      double_stream << best_dist;
+      const char *double_str = double_stream.str().c_str();
+      json::Value dist(double_str, allocator);
+      return_json.AddMember("vertex", dist, allocator);
+    }
+  }
   
-  json::Document return_json;
-  return_json.SetObject();
+  void scatter(json::Document& invocation, json::Document& return_json){
+    json::Document::AllocatorType& allocator = return_json.GetAllocator();
+    // best distance found for this vertex
+    double best_dist = stof(invocation["params"]["vertex"]["state"].GetString());
+    // parse distances
+    const json::Value& edge = invocation["params"]["edge"];
+    double nbr_dist = stof(edge["target"]["state"].GetString());
+    double edge_dist = atof(edge["state"].GetString());
+    // relax edge
+    if (best_dist < INFINITY && nbr_dist > (best_dist + edge_dist))
+      return_json.AddMember("signal", "TARGET", allocator);
+  }
   
-  if (!strcmp(invocation["method"].GetString(), "init"))
-    init(invocation, return_json);
-  
-  if (!strcmp(invocation["method"].GetString(), "gather_edges"))
-    gather_edges(invocation, return_json);
-    
-  if (!strcmp(invocation["method"].GetString(), "scatter_edges"))
-    scatter_edges(invocation, return_json);
-    
-  if (!strcmp(invocation["method"].GetString(), "gather"))
-    gather(invocation, return_json);
-  
-  if (!strcmp(invocation["method"].GetString(), "merge"))
-    merge(invocation, return_json);
-  
-  if (!strcmp(invocation["method"].GetString(), "apply"))
-    apply(invocation, return_json);
-  
-  if (!strcmp(invocation["method"].GetString(), "scatter"))
-    scatter(invocation, return_json);
-  
-  return_buffer.Clear();
-  json::Writer<json::StringBuffer> writer(return_buffer);
-  return_json.Accept(writer);
-  return return_buffer.GetString();
-  
-}
+};
 
 int main(int argc, char** argv) {
 
-  std::string line;
-  std::size_t length = 0;
-  std::size_t current_length = INITIAL_LENGTH;
-  char *buffer = new char[current_length];
-  
-  // loop until exit is received
-  while (true){
-    
-    // TODO: does this process know if the other end closes the pipe?
-    
-    // TODO: assume NULL-terminated string for now
-    std::cin >> length; std::getline(std::cin, line);
-    if (length + 1 > current_length){
-      current_length = length + 1;
-      delete[] buffer;
-      buffer = new char[current_length];
-    }
-    
-    // read message, break if exit
-    std::cin.read(buffer, length);
-    buffer[length] = '\0';    // terminate string w. null
-    json::StringBuffer return_buffer;
-    
-    std::cerr << buffer << std::endl;
-    const char *return_json = handle_invocation(buffer, return_buffer);
-    if (!return_json) break;
-    
-    // return
-    std::cout << strlen(return_json) << "\n";
-    std::cout << return_json << std::flush;
-    
-  }
-  
-  delete[] buffer;
+  cgi_handler handler;
+  graphlab::cgi client(handler); 
+  client.listen();
   return 0;
 
 }
