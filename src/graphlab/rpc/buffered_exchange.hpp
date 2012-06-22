3c04af84c8951dce7e95b36987141bf0124830ac
Yucheng Low <yuchenglow@gmail.com>
2012-06-22 17:12:12 -0400

    Performance improvements in buffered exchange

--- src/graphlab/rpc/buffered_exchange.hpp
+++ src/graphlab/rpc/buffered_exchange.hpp
@@ -45,8 +45,9 @@ namespace graphlab {
   private:
     struct buffer_record {
       procid_t proc;
-      buffer_type buffer;
-      buffer_record() : proc(-1) { }
+      size_t numel;
+      dc_impl::blob buffer;
+      buffer_record() : proc(-1), numel(0) { }
     }; // end of buffer record
 
 
@@ -57,7 +58,17 @@ namespace graphlab {
     std::deque< buffer_record > recv_buffers;
     mutex recv_lock;
 
-    std::vector< buffer_type > send_buffers;
+
+    struct send_record {
+      send_record():buffer(128),numinserts(0){}
+      // need a fake copy constructor here
+      // just so I can make a vector of these
+      send_record(const send_record& ):buffer(128), numinserts(0) { }
+      charstream buffer;
+      size_t numinserts;
+    };
+
+    std::vector<send_record> send_buffers;
     std::vector< mutex >  send_locks;
     const size_t num_threads;
     const size_t max_buffer_size;
@@ -69,13 +80,23 @@ namespace graphlab {
   public:
     buffered_exchange(distributed_control& dc, 
                       const size_t num_threads = 1, 
-                      const size_t max_buffer_size = 1000) : 
+                      const size_t max_buffer_size = 1000000) : 
       rpc(dc, this), 
       send_buffers(num_threads *  dc.numprocs()), 
       send_locks(num_threads *  dc.numprocs()),
       num_threads(num_threads),
-      max_buffer_size(max_buffer_size) { rpc.barrier(); }
+      max_buffer_size(max_buffer_size) { 
+       rpc.barrier(); 
+      }
 
+
+
+    ~buffered_exchange() { 
+     foreach(buffer_record& buf, recv_buffers) {
+        buf.buffer.free();
+      }
+
+    }
     // buffered_exchange(distributed_control& dc, handler_type recv_handler, 
     //                   size_t buffer_size = 1000) : 
     // rpc(dc, this), send_buffers(dc.numprocs()), send_locks(dc.numprocs()),
@@ -88,15 +109,25 @@ namespace graphlab {
       const size_t index = thread_id * rpc.numprocs() + proc;
       ASSERT_LT(index, send_locks.size());
       send_locks[index].lock();
-      send_buffers[index].push_back(value);
-      if(send_buffers[index].size() > max_buffer_size) {
+
+      oarchive oarc(send_buffers[index].buffer);
+      ++send_buffers[index].numinserts;
+      oarc << value;
+      if(send_buffers[index].buffer->size() > max_buffer_size) {
+        send_buffers[index].buffer.flush();
+        graphlab::dc_impl::blob b(send_buffers[index].buffer->str, 
+                                  send_buffers[index].buffer->len);
         if(proc == rpc.procid()) {
-          rpc_recv(proc, send_buffers[index]);
+          rpc_recv(proc, send_buffers[index].numinserts, b);
+          // here the blob is transimtted directly
         } else {
           rpc.remote_call(proc, &buffered_exchange::rpc_recv,
-                          rpc.procid(), send_buffers[index]);
+                          rpc.procid(), send_buffers[index].numinserts, b);
+          // here I need to free the blob
+          b.free();
         }
-        send_buffers[index].clear();
+        send_buffers[index].buffer->relinquish();
+        send_buffers[index].numinserts = 0;
       }
       send_locks[index].unlock();
     } // end of send
@@ -107,14 +138,18 @@ namespace graphlab {
         const procid_t proc = i % rpc.numprocs();
         ASSERT_LT(proc, rpc.numprocs());
         send_locks[i].lock();
+        send_buffers[i].buffer.flush();
+        graphlab::dc_impl::blob b(send_buffers[i].buffer->str, 
+                                  send_buffers[i].buffer->len);
         if(proc == rpc.procid()) {
-          rpc_recv(proc, send_buffers[i]);
+          rpc_recv(proc, send_buffers[i].numinserts, b);
         } else {
           rpc.remote_call(proc, &buffered_exchange::rpc_recv,
-                          rpc.procid(), send_buffers[i]);
+                          rpc.procid(),send_buffers[i].numinserts, b);
+          b.free();
         }
-        send_buffers[i].clear();
-        buffer_type().swap(send_buffers[i]);
+        send_buffers[i].buffer->relinquish();
+        send_buffers[i].numinserts = 0;
         send_locks[i].unlock();
       }
       rpc.full_barrier();
@@ -123,6 +158,8 @@ namespace graphlab {
 
     bool recv(procid_t& ret_proc, buffer_type& ret_buffer, 
               const bool try_lock = false) {
+      dc_impl::blob read_buffer;
+      size_t numel = 0;
       bool has_lock = false;
       if(try_lock) {
         has_lock = recv_lock.try_lock();
@@ -135,11 +172,26 @@ namespace graphlab {
         if(!recv_buffers.empty()) {
           success = true;
           buffer_record& rec =  recv_buffers.front();
-          ret_proc = rec.proc; ret_buffer.swap(rec.buffer);
+          // read the record 
+          ret_proc = rec.proc; 
+          read_buffer = rec.buffer;
+          numel = rec.numel;
           ASSERT_LT(ret_proc, rpc.numprocs());
           recv_buffers.pop_front();
         }
         recv_lock.unlock();
+      }
+
+     ret_buffer.clear();
+      if (success) {
+        // deserialize tmp_buffer
+        boost::iostreams::stream<boost::iostreams::array_source> strm(read_buffer.c, read_buffer.len);
+        iarchive iarc(strm);
+        ret_buffer.resize(numel);
+        for (size_t i = 0;i < numel; ++i) {
+          iarc >> ret_buffer[i];
+        }
+        read_buffer.free();
       }
       return success;
     } // end of recv
@@ -153,8 +205,9 @@ namespace graphlab {
       typedef typename std::deque< buffer_record >::const_iterator iterator;
       recv_lock.lock();
       size_t count = 0;
-      foreach(const buffer_record& rec, recv_buffers) 
-        count += rec.buffer.size();
+      foreach(const buffer_record& rec, recv_buffers) {
+        count += rec.buffer.len;
+      }
       recv_lock.unlock();
       return count;
     } // end of size
@@ -162,19 +215,19 @@ namespace graphlab {
     bool empty() const { return recv_buffers.empty(); }
 
     void clear() {
-      std::vector<buffer_type>().swap(send_buffers);
-      send_buffers.resize(rpc.numprocs() * num_threads);
-      std::deque<buffer_record>().swap(recv_buffers);
-      recv_buffers.resize(rpc.numprocs() * num_threads);
+      foreach(buffer_record& buf, recv_buffers) {
+        buf.buffer.free();
+      }
     }
 
   private:
-    void rpc_recv(procid_t src_proc, buffer_type& buffer) {
+    void rpc_recv(procid_t src_proc, size_t numel, dc_impl::blob& buffer) {
       recv_lock.lock();
       recv_buffers.push_back(buffer_record());
       buffer_record& rec = recv_buffers.back();
       rec.proc = src_proc;
-      rec.buffer.swap(buffer);
+      rec.numel = numel;
+      rec.buffer = buffer;
       recv_lock.unlock();
     } // end of rpc rcv
 
