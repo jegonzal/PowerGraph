44ac32de02818c2bdc4d09203a4687ec1ed60aaf
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2012-07-02 13:00:45 -0400

    Substantially improved documentation of ivertex_program interface

--- src/graphlab/vertex_program/ivertex_program.hpp
+++ src/graphlab/vertex_program/ivertex_program.hpp
@@ -140,6 +140,125 @@ namespace graphlab {
    * serializable (see \ref sec_serializable) and default
    * constructible to enable movement between machines.
    *
+   * Advanced Features
+   * ======================
+   *
+   * While the basic Gather-Apply-Scatter approach to graph structure
+   * computation can express a wide range of algorithms there are some
+   * situation where additional features could either simplify the
+   * design or provide additional efficiency. 
+   *
+   *
+   * Messaging 
+   * ----------------------
+   * 
+   * Vertex-programs can trigger adjacent vertex programs by sending a
+   * signal which can contain a message to neighbor vertices.  By
+   * default the message type is empty however it is possible for the
+   * user to define a message type.  For example the following
+   * message_type could be used to implement pagerank:
+   *
+   * \code
+   * struct pagerank_message : public graphlab::IS_POD_TYPE {
+   *   double value;
+   *   double priority() const { return std::fabs(value); }
+   *   message_type& operator+=(const message_type& other) {
+   *     value += other.value; 
+   *     return *this;
+   *   }
+   * }; 
+   * \endcode
+   *
+   * Unlike other messaging abstractions, GraphLab always _merges_
+   * messages destined to the same vertex.  This allows the GraphLab
+   * engines to minimize network communication and more evenly balance
+   * computation.  Messages are combined using the operator+=
+   * function.
+   *
+   * As mentioned earlier some engines may prioritize the _reception_
+   * of messages.  Messages can optionally (it is not required)
+   * provide a priority function which is used to prioritize message
+   * reception.  The engine then attempts to prioritize the reception
+   * of higher priority messages first.
+   *
+   * The message is received in the \ref ivertex_program::init
+   * function.  The single message passed into 
+   * \ref ivertex_program::init represents the sum of all messages 
+   * destined to that vertex since the vertex-program was last invoked.
+   * 
+   * The GraphLab messaging framework allows us to write
+   * Pregel-like vertex-programs of the form:
+   *
+   * \code
+   * typedef graphlab::empty gather_type;
+   * class pregel_pagerank : 
+   *   public ivertex_program<graph_type, gather_type, pagerank_message>,
+   *   public graphlab::IS_POD_TYPE {
+   *
+   *   // Store a local copy of the message data
+   *   double message_value;
+   *
+   *   // Receive the inbound message (sum of messages)
+   *   void init(icontext_type& context, const vertex_type& vertex, 
+   *             const message_type& msg) { 
+   *     message_value = message.value;
+   *   }
+   *
+   *   // Skip the gather phase
+   *   edge_dir_type gather_edges(icontext_type& context,
+   *                              const vertex_type& vertex) const { 
+   *     return graphlab::NO_EDGES; 
+   *   }
+   *
+   *   // Update the pagerank using the message
+   *   void apply(icontext_type& context, vertex_type& vertex, 
+   *              const gather_type& total) {
+   *     vertex.data() += message_value;      
+   *   }
+   *
+   *   // Scatter along out edges
+   *   edge_dir_type scatter_edges(icontext_type& context,
+   *                               const vertex_type& vertex) const { 
+   *     return OUT_EDGES; 
+   *   }
+   *
+   *   // Compute new messages encoding the change in the pagerank of
+   *   // adjacent vertices.
+   *   void scatter(icontext_type& context, const vertex_type& vertex, 
+   *                edge_type& edge) const { 
+   *     context.signal(edge.target(), message_value * (1-RESET_PROBABILITY));
+   *   }
+   * }; 
+   * \endcode
+   *
+   * Notice that the gather phase is skipped and instead the gather
+   * computation is accomplished using the messages.  However unlike
+   * Pregel the scatter function which computs and sends the new
+   * message is actually run on the machine that is receiving the
+   * message.  
+   *
+   * The message abstraction is surprisingly powerful and can often
+   * often express computation that can be written using the Gather
+   * operation.  However, the message combination is done outside of
+   * the consistency model and so can lead to more confusing code.
+   *
+   * Gather Caching
+   * ---------------------
+   *
+   * In many applications the gather computation can be costly and
+   * high-degree vertices will be signaled often even only a small
+   * fraction of its neighbors values have changed.  In this case
+   * running the gather function on all neighbors can be wasteful.  To
+   * address this important issue the GraphLab engines expose a gather
+   * caching mechanism.  However to take advantage of the gather
+   * caching the vertex-program must notify the engine when a cache is
+   * no longer valid and can even correct the cache to ensure that it
+   * remains valid.
+   *
+   * \todo finish documenting gather caching
+   *
+   * 
+   * 
    */
   template<typename Graph,
            typename GatherType,
@@ -325,71 +444,178 @@ namespace graphlab {
 
     // Graph specific type members ============================================
     /**
-     * The graph type associative with this vertex program.
+     * \brief The graph type associative with this vertex program. 
+     *
+     * The graph type is specified as a template argument and will
+     * usually be \ref distributed_graph.
      */
     typedef Graph graph_type;
 
     /**
-     * The unique integer id used to reference vertices in the graph.
-     * TODO: add doc link to definition in graph_type
+     * \brief The unique integer id used to reference vertices in the graph.
+     * 
+     * See \ref graphlab::vertex_id_type for details.
      */
     typedef typename graph_type::vertex_id_type vertex_id_type;
     
     /**
-     * The opaque vertex object type used to get vertex information.
-     * TODO: add doc link to definition in graph_type
+     * \brief The opaque vertex object type used to get vertex
+     * information.
+     *
+     * The vertex type is defined by the graph.  
+     * See \ref distributed_graph::vertex_type for details.
      */
     typedef typename graph_type::vertex_type vertex_type;
     
     /**
-     * The opaque edge_object type used to access edge information.
-     * TODO: add doc link to definition in graph_type
+     * \brief The opaque edge_object type used to access edge
+     * information.
+     * 
+     * The edge type is defined by the graph.  
+     * See \ref distributed_graph::edge_type for details.
      */
     typedef typename graph_type::edge_type edge_type;
 
     /**
-     * The type used to define the direction of edges used in gather
-     * and scatter.
-     * TODO: add doc link to definition in graph_type
+     * \brief The type used to define the direction of edges used in
+     * gather and scatter.
+     * 
+     * Possible values include:
+     *
+     * \li graphlab::NO_EDGES : Do not process any edges
+     * 
+     * \li graphlab::IN_EDGE : Process only inbound edges to this
+     * vertex
+     * 
+     * \li graphlab::OUT_EDGES : Process only outbound edges to this
+     * vertex
+     *
+     * \li graphlab::ALL_EDGES : Process both inbound and outbound
+     * edges on this vertes.
+     * 
+     * See \ref graphlab::edge_dir_type for details.
      */
     typedef graphlab::edge_dir_type edge_dir_type;
 
     // Additional Types =======================================================
     
     /**
-     * The context type is used by the vertex program to communicate
-     * with the engine and provides facilities for sending messages,
-     * posting deltas, and accessing engine state.
+     * \brief The context type is used by the vertex program to
+     * communicate with the engine.
+     *
+     * The context and provides facilities for signaling adjacent
+     * vertices (sending messages), interacting with the GraphLab
+     * gather cache (posting deltas), and accessing engine state.
+     *
      */
     typedef icontext<graph_type, gather_type, message_type> icontext_type;
    
     // Functions ==============================================================
+    /**
+     * \brief Standard virtual destructor for an abstract class.
+     */
     virtual ~ivertex_program() { }
 
     /**
-     * init() is called by the engine to receive a message to
-     * this vertex program.  The vertex program can use this to
-     * initialize any state before entering the gather phase.  If the
-     * vertex program does not implement this function then the
+     * \brief This called by the engine to receive a message to this
+     * vertex program.  The vertex program can use this to initialize
+     * any state before entering the gather phase.  The init function
+     * is invoked _once_ per execution of the vertex program.
+     *
+     * If the vertex program does not implement this function then the
      * default implementation (NOP) is used.
+     *
+     * \param [in,out] context The context is used to interact with the engine
+     *
+     * \param [in] vertex The vertex on which this vertex-program is
+     * running. Note that the vertex is constant and its value should
+     * not be modified within the init function.  If there is some
+     * message state that is needed then it must be saved to the
+     * vertex-program and not the vertex data.
+     *
+     * \param [in] message The sum of all the signal calls to this
+     * vertex since it was last run.
      */
     virtual void init(icontext_type& context,
                       const vertex_type& vertex, 
                       const message_type& msg) { /** NOP */ }
     
+
     /**
-     * Returns the set of edges on which to run the gather function.
-     * The default edge direction is the in edges.
+     * \brief Returns the set of edges on which to run the gather
+     * function.  The default edge direction is out edges.
+     *
+     * The gather_edges function is invoked after the init function
+     * has completed.
+     *
+     * \warning The gather_edges function may be invoked multiple
+     * times for the same execution of the vertex-program and should
+     * return the same value.  In addition it cannot modify the
+     * vertex-programs state or the vertex data.
+     *
+     * Possible return values include:
+     *
+     * \li graphlab::NO_EDGES : The gather phase is completely skipped
+     * potentially reducing network communication.
+     * 
+     * \li graphlab::IN_EDGE : The gather function is only run on
+     * inbound edges to this vertex.
+     * 
+     * \li graphlab::OUT_EDGES : The gather function is only run on
+     * outbound edges to this vertex.
+     *
+     * \li graphlab::ALL_EDGES : The gather function is run on both
+     * inbound and outbound edges to this vertes.
+     * 
+     * \param [in,out] context The context is used to interact with
+     * the engine
+     *
+     * \param [in] vertex The vertex on which this vertex-program is
+     * running. Note that the vertex is constant and its value should
+     * not be modified.
+     * 
+     * \return One of graphlab::NO_EDGES, graphlab::IN_EDGES,
+     * graphlab::OUT_EDGES, or graphlab::ALL_EDGES.
+     * 
      */
     virtual edge_dir_type gather_edges(icontext_type& context,
                                        const vertex_type& vertex) const { 
       return IN_EDGES; 
     }
 
+
     /**
-     * Gather is called on all gather_edges() in parallel and returns
-     * the gather_type which are added to compute the final output of
-     * the gather.
+     * \brief The gather function is called on all the 
+     * \ref ivertex_program::gather_edges in parallel and returns the 
+     * \ref gather_type which are added to compute the final output of 
+     * the gather phase.  
+     *
+     * The gather function is the core computational element of the
+     * Gather phase and is responsible for collecting the information
+     * about the state of adjacent vertices and edges.  
+     *
+     * \warning The gather function is executed in parallel on
+     * multiple machines and therefore cannot modify the
+     * vertex-program's state or the vertex data.
+     *
+     * A default implementation of the gather function is provided
+     * which will fail if invoked. 
+     *
+     * \param [in,out] context The context is used to interact with
+     * the engine
+     *
+     * \param [in] vertex The vertex on which this vertex-program is
+     * running. Note that the vertex is constant and its value should
+     * not be modified.
+     *
+     * \param [in,out] edge The adjacent edge to be processed.  The
+     * edge is not constant and therefore the edge data can be
+     * modified. 
+     *
+     * \return the result of the gather computation which will be
+     * "summed" to produce the input to the apply operation.  The
+     * behavior of the "sum" is defined by the \ref gather_type.
+     * 
      */
     virtual gather_type gather(icontext_type& context, 
                                const vertex_type& vertex, 
@@ -398,17 +624,67 @@ namespace graphlab {
       return gather_type();
     };
 
+
     /**
-     * The apply function is called once the gather has completed and
-     * must be implemented by all vertex programs. 
+     * \brief The apply function is called once the gather has
+     * completed and must be implemented by all vertex programs.
+     * 
+     * The apply function is responsible for modifying the vertex data
+     * and is run only once per vertex per execution of a vertex
+     * program.  In addition the apply function can modify the state
+     * of the vertex program.
+     *
+     * \param [in,out] context The context is used to interact with
+     * the engine
+     *
+     * \param [in,out] vertex The vertex on which this vertex-program is
+     * running. 
+     *
+     * \param [in] total The result of the gather phase.  If a vertex
+     * has no neighbors than the total is the default value (i.e.,
+     * gather_type()) of the gather type.
+     * 
      */
     virtual void apply(icontext_type& context, 
                        vertex_type& vertex, 
                        const gather_type& total) = 0;
 
     /**
-     * Returns the set of edges on which to run the scatter function.
-     * The default edge direction is the out edges.
+     * \brief Returns the set of edges on which to run the scatter
+     * function.  The default edge direction is out edges.
+     *
+     * The scatter_edges function is invoked after the apply function
+     * has completed.
+     *
+     * \warning The scatter_edges function may be invoked multiple
+     * times for the same execution of the vertex-program and should
+     * return the same value.  In addition it cannot modify the
+     * vertex-programs state or the vertex data.
+     *
+     * Possible return values include:
+     *
+     * \li graphlab::NO_EDGES : The scatter phase is completely
+     * skipped potentially reducing network communication.
+     * 
+     * \li graphlab::IN_EDGE : The scatter function is only run on
+     * inbound edges to this vertex.
+     * 
+     * \li graphlab::OUT_EDGES : The scatter function is only run on
+     * outbound edges to this vertex.
+     *
+     * \li graphlab::ALL_EDGES : The scatter function is run on both
+     * inbound and outbound edges to this vertes.
+     * 
+     * \param [in,out] context The context is used to interact with
+     * the engine
+     *
+     * \param [in] vertex The vertex on which this vertex-program is
+     * running. Note that the vertex is constant and its value should
+     * not be modified.
+     * 
+     * \return One of graphlab::NO_EDGES, graphlab::IN_EDGES,
+     * graphlab::OUT_EDGES, or graphlab::ALL_EDGES.
+     * 
      */
     virtual edge_dir_type scatter_edges(icontext_type& context,
                                         const vertex_type& vertex) const { 
@@ -416,9 +692,33 @@ namespace graphlab {
     }
 
     /**
-     * Scatter is called on all scatter_edges() in parallel after the
-     * apply function has completed.  The scatter function can post
-     * deltas.
+     * \brief Scatter is called on all scatter_edges() in parallel
+     * after the apply function has completed and is typically
+     * responsible for updating edge data, signaling (messaging)
+     * adjacent vertices, and updating the gather cache state when
+     * caching is enabled.
+     *
+     * The scatter function is almost identical to the gather function
+     * except that nothing is returned. 
+     *
+     * \warning The scatter function is executed in parallel on
+     * multiple machines and therefore cannot modify the
+     * vertex-program's state or the vertex data.
+     *
+     * A default implementation of the gather function is provided
+     * which will fail if invoked. 
+     *
+     * \param [in,out] context The context is used to interact with
+     * the engine
+     *
+     * \param [in] vertex The vertex on which this vertex-program is
+     * running. Note that the vertex is constant and its value should
+     * not be modified.
+     *
+     * \param [in,out] edge The adjacent edge to be processed.  The
+     * edge is not constant and therefore the edge data can be
+     * modified. 
+     *
      */
     virtual void scatter(icontext_type& context, const vertex_type& vertex, 
                          edge_type& edge) const { 
