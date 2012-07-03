40689fab78655fd15ae7c926b39ca2a96e7a7beb
Yucheng Low <yuchenglow@gmail.com>
2012-07-03 10:07:03 -0400

    Removed linear solvers, parsers and "shared" from the toolkits folder

--- toolkits/parsers/convert_mm_to_reverse.m
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -f $1.reverse
-head -n 1 $1 > $1.reverse
-head -n 3 $1 | grep -v "%" | head -n 1 | awk '{print $1 " " $2 " " $3 }' >> $1.reverse
-cat $1 | grep -v "%" | sort -n -k 2,2 -k 1,1 -T . >> $1
