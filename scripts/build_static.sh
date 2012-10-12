a21f0740af6abe33e5eff5d6708229fbef8971a0
Yucheng Low <yuchenglow@gmail.com>
2012-10-12 16:12:59 -0700

    - Added an external_dependencies target which will build all core external dependencies (excluding opencv)  - Make libhdfs build static and dynamic and drop the "native" build option  - Added a build_static.sh script which will force static builds for almost all core external dependencies

---
+++ scripts/build_static.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -d release ]; then
+  echo "Run from the graphlab root folder after ./configure"
+else
+  cd release
+  make external_dependencies
+  cd ..
+  rm -f deps/local/lib/libboost*.so deps/local/lib/libhdfs*.so deps/local/lib/libtcmalloc*.so deps/local/lib/libevent*.so
+  rm -f deps/local/lib/libboost*.dylib deps/local/lib/libhdfs*.dylib deps/local/lib/libtcmalloc*.dylib deps/local/lib/libevent*.dylib
+  cd release
+  make
+fi
