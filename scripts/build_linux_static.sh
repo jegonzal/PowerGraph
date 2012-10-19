3f19afd177a6068f389d67622f5fd6b0d9b88768
Yucheng Low <yuchenglow@gmail.com>
2012-10-19 00:06:30 -0700

    Various binary build scripts

---
+++ scripts/build_linux_static.sh
@@ -0,0 +1,78 @@
+if [ ! -d src ]; then
+  echo "Run from the graphlab root folder"
+  exit
+fi
+pushd .
+cd release/toolkits
+make clean
+popd
+./configure -D MARCH=x86-64 -D MTUNE=generic
+scripts/compile_static_release.sh
+
+# is this a openmpi or a mpich2 build?
+if grep -q openmpi config.log
+then
+  rootdirname="graphlab_openmpi"
+  unstrippeddirname="graphlab_openmpi_unstripped"
+elif grep -q mpich2 config.log
+then
+  rootdirname="graphlab_mpich2"
+  unstrippeddirname="graphlab_mpich2_unstripped"
+else
+  echo "Unable to detect MPI type"
+  exit
+fi
+
+
+# now package a binary release
+rm -rf ./$rootdirname
+rm -rf ./$unstrippeddirname
+mkdir $rootdirname
+mkdir $unstrippeddirname
+mkdir $rootdirname/gldeps
+mkdir $unstrippeddirname/gldeps
+
+for file in `cat scripts/binary_list.txt`
+do
+  dname=`dirname $file`
+  fname=`basename $file`
+
+  deps=$(ldd $file | awk 'BEGIN{ORS=" "}$1 \
+      ~/^\//{print $1}$3~/^\//{print $3}' \
+       | sed 's/,$/\n/')
+
+  for dep in $deps
+  do
+    depname=`basename $dep`
+    if [ ! -a "$rootdirname/gldeps/$depname" ]; then
+      echo "Copying $dep"
+      cp "$dep" "$rootdirname/gldeps/"
+      cp "$dep" "$unstrippeddirname/gldeps/"
+    fi
+  done
+
+  mkdir -p $rootdirname/$dname
+  cp release/$file $rootdirname/$dname/
+  #strip it
+  strip $rootdirname/$dname/$fname
+  #package the script
+  cp scripts/linux_run_script_template.sh $rootdirname/$dname/$fname.sh
+
+  #repeat for unstripped
+  mkdir -p $unstrippeddirname/$dname
+  cp release/$file $unstrippeddirname/$dname/
+  #package the script
+  cp scripts/linux_run_script_template.sh $unstrippeddirname/$dname/$fname.sh
+done
+
+#package all the rest of the stuff
+#copy the license
+mkdir $rootdirname/license
+cp license/LICENSE.txt $rootdirname/license/
+
+mkdir $unstrippeddirname/license
+cp license/LICENSE.txt $unstrippeddirname/license/
+
+#copy the README
+cp BINARY_README $rootdirname/
+cp BINARY_README $unstrippeddirname/
--- scripts/build_osx_static.sh
+++ scripts/build_osx_static.sh
@@ -1,6 +1,33 @@
 if [ ! -d src ]; then
   echo "Run from the graphlab root folder"
-else
-  ./configure --no_jvm -D NO_MPI:BOOL=true -D COMPILER_FLAGS="-mmacosx-version-min=10.4" -D MARCH=x86-64 -D MTUNE=generic
-  scripts/build_static.sh
+  exit
 fi
+
+pushd .
+cd release/toolkits
+make clean
+popd
+
+./configure --no_jvm -D NO_MPI:BOOL=true -D COMPILER_FLAGS="-mmacosx-version-min=10.4" -D MARCH=x86-64 -D MTUNE=generic
+scripts/compile_static_release.sh
+
+# now package a binary release
+# for whatever reason the mac binaries are quite small... 
+# stripping not necessary
+rootdirname="graphlab_mac"
+rm -rf ./$rootdirname
+mkdir $rootdirname
+for file in `cat scripts/binary_list.txt`
+do
+  dname=`dirname $file`
+  mkdir -p $rootdirname/$dname
+  cp release/$file $rootdirname/$dname/
+done
+
+#package all the rest of the stuff
+#copy the license
+mkdir $rootdirname/license
+cp license/LICENSE.txt $rootdirname/license/
+
+#copy the README
+cp BINARY_README $rootdirname/
