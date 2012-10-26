97b64b7031815b0d21aee7a0b201fee262beb78a
Andre Martins <andre.t.martins@gmail.com>
2012-10-26 18:35:17 +0100

    print some debug messages

--- src/graphlab/graph/distributed_graph.hpp
+++ src/graphlab/graph/distributed_graph.hpp
@@ -788,6 +788,9 @@ namespace graphlab {
           << std::endl;
       }
       ASSERT_NE(ingress_ptr, NULL);
+
+      std::cout << "inside add_edge" << std::endl;
+        
       ingress_ptr->add_edge(source, target, edata);
     }
 
