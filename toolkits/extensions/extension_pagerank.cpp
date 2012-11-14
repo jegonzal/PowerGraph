1ee189c5996cbc8152d25da5c9dd015934430be9
Yucheng Low <yuchenglow@gmail.com>
2012-11-14 00:39:32 -0800

    extension_pagerank: Added support to drop the output argument. Output printing from within the pagerank_extension driver

--- toolkits/extensions/extension_pagerank.cpp
+++ toolkits/extensions/extension_pagerank.cpp
@@ -4,16 +4,21 @@ namespace graphlab {
 namespace extension {
 
 void pagerank(extension_graph& graph, 
-              const std::string PR_FIELD,
+              const std::string PR_FIELD_NAME,
               double tolerance) {
-  const std::string PR_CHANGE = PR_FIELD + "_change";
+  const std::string PR_CHANGE_NAME = PR_FIELD_NAME + "_change";
+
+  key_id_type PR_FIELD = get_id_from_name(PR_FIELD_NAME);
+  key_id_type PR_CHANGE = get_id_from_name(PR_CHANGE_NAME);
+  key_id_type OUT_DEG = get_id_from_name("out_degree");
 
   graph.transform_field(PR_FIELD, [](var v){ return 0.15; });    
+  timer ti;
   graph.GAS(
       [](const vars&) { return graphlab::IN_EDGES; },             // gather_edges
       [=](const vars&, vars&, const vars& other, edge_direction) { // gather
           return get<double>(other(PR_FIELD)) / 
-                get<double>(other("out_degree")) ;
+                get<double>(other(OUT_DEG)) ;
       }, 
       [](var& a, const var& b) {                                  // combine
           get<double>(a) += get<double>(b); 
@@ -22,7 +27,7 @@ void pagerank(extension_graph& graph,
           double pr = 0.15 + 0.85 * get<double>(result); 
           v(PR_CHANGE) = 
               std::fabs(pr - get<double>(v(PR_FIELD))) / 
-              get<double>(v("out_degree"));
+              get<double>(v(OUT_DEG));
           v(PR_FIELD) = pr;         
           return false; 
       }, 
@@ -34,6 +39,7 @@ void pagerank(extension_graph& graph,
           return true; 
       }
   ); // scatter
+  std::cout << "PageRank complete in " << ti.current_time() << "s" << std::endl;
 }
 
 
--- toolkits/extensions/pagerank_extension_driver.cpp
+++ toolkits/extensions/pagerank_extension_driver.cpp
@@ -4,13 +4,13 @@ using namespace graphlab::extension;
 
 int main(int argc, char** argv) {
   extension_graph graph;
-  if (argc < 3) {
-    std::cout << argv[0] << " [input prefix] [output prefix]\n"; 
+  if (argc < 2) {
+    std::cout << argv[0] << " [input prefix] optional:[output prefix]\n"; 
     return 0;
   }
   graph.load_structure(argv[1], "snap");
-  graphlab::timer ti;
   pagerank(graph, "pr", 0.01);
-  std::cout << "PageRank in " << ti.current_time() << "s\n";
-  graph.save_vertices(argv[2], "pr");
+  if (argc > 2) {
+    graph.save_vertices(argv[2], "pr");
+  }
 }
