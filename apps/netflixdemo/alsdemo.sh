a5d441eb043028eb9605ecd2d139e1894eeefc59
Haijie Gu <gu.haijie@gmail.com>
2013-04-02 15:47:19 -0400

    remove unnecessary scripts

--- apps/netflixdemo/alsdemo.sh
+++ apps/netflixdemo/alsdemo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cmd="mpiexec -np 12 -hostfile $HOME/machines ../../release/apps/netflixdemo/als_netflix_demo --matrix=$HOME/data/netflix/split --movielist=$HOME/data/netflix/moviename.txt --engine=sync --max_iter=3 --D=20"
+cmd="mpiexec -np 12 -hostfile $HOME/machines env CLASSPATH=`hadoop classpath` ../../release/apps/netflixdemo/als_netflix_demo --matrix=$HOME/data/netflix/split --movielist=$HOME/data/netflix/moviename.txt --engine=sync --max_iter=3 --D=20 --implicitratingweight=1 --implicitratingvalue=3 --implicitratingpercentage=0.01 --users=480189 --items=17770 --implicitratingtype=1"
 echo $cmd
 $cmd
