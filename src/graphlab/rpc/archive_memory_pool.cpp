902d72d2285ab30d9454d0b4036a1464fc30b2b6
Yucheng Low <yuchenglow@gmail.com>
2013-04-19 19:17:10 +0000

    Improved multithreaded RPC performance dramatically

--- src/graphlab/rpc/archive_memory_pool.cpp
+++ src/graphlab/rpc/archive_memory_pool.cpp
@@ -1,43 +1,56 @@
 #include <graphlab/util/lock_free_pool.hpp>
 #include <graphlab/rpc/dc_compile_parameters.hpp>
 #include <vector>
-
+#include <utility>
 namespace graphlab {
 namespace dc_impl {
 struct pool_manager {
-  lock_free_pool<oarchive> pool;
-
-  pool_manager(){
-    pool.reset_pool(64);
-    std::vector<oarchive>& r = pool.unsafe_get_pool_ref();
-    for (size_t i = 0;i < r.size(); ++i) {
-      r[i].buf = (char*)malloc(BUFFER_RELINQUISH_LIMIT);
-      r[i].off = 0;
-      r[i].len = BUFFER_RELINQUISH_LIMIT;
+  pthread_key_t thlocalkey;
+  typedef std::pair<oarchive, bool> thlocal_type;
+  static void pth_deleter(void* p) {
+    if (p != NULL) {
+      thlocal_type* arc = (thlocal_type*)(p);
+      if (arc->first.buf) free(arc->first.buf);
+      delete arc;
     }
+  }
+  pool_manager(){
+    pthread_key_create(&thlocalkey, &pth_deleter);
   }
 
   ~pool_manager() {
-    std::vector<oarchive>& r = pool.unsafe_get_pool_ref();
-    for (size_t i = 0;i < r.size(); ++i) {
-      if (r[i].buf) free(r[i].buf);
-    }
+    pthread_key_delete(thlocalkey);
   }
 };
 
 static pool_manager pool;
 
 oarchive* oarchive_from_pool() {
-  return pool.pool.alloc();
+  void* ptr = pthread_getspecific(pool.thlocalkey);
+  pool_manager::thlocal_type* p = (pool_manager::thlocal_type*)(ptr);
+  if (p == NULL) {
+    p = new pool_manager::thlocal_type;
+    p->second = false;
+    p->first.buf = (char*)malloc(BUFFER_RELINQUISH_LIMIT);
+    p->first.off = 0;
+    p->first.len = BUFFER_RELINQUISH_LIMIT;
+    pthread_setspecific(pool.thlocalkey, (void*)p);
+  }
+  if (p->second) return new oarchive;
+  p->second = true;
+  return &(p->first);
 }
 
 void release_oarchive_to_pool(oarchive* oarc) {
-  oarc->off = 0;
-  if (oarc->buf == NULL) {
-    oarc->buf = (char*)malloc(BUFFER_RELINQUISH_LIMIT);
-    oarc->len = BUFFER_RELINQUISH_LIMIT;
+  void* ptr = pthread_getspecific(pool.thlocalkey);
+  pool_manager::thlocal_type* p = (pool_manager::thlocal_type*)(ptr);
+  if ((p != NULL) && &(p->first) == oarc) {
+    oarc->off = 0;
+    p->second = false;
+  } else {
+    if (oarc->buf) free(oarc->buf);
+    delete oarc;
   }
-  pool.pool.free(oarc);
 }
 
 
