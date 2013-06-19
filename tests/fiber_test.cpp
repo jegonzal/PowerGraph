f11e2faca98073335d62862dbb08e6b28f265f74
Yucheng Low <ylow@graphlab.com>
2013-06-19 13:54:27 -0700

    Renamed fiber_group to fiber_control. Introduced a fiber_control singleton accessible via fiber_control::get_instance() Updated the tests

--- tests/fiber_test.cpp
+++ tests/fiber_test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <graphlab/parallel/fiber.hpp>
+#include <graphlab/parallel/fiber_control.hpp>
 #include <graphlab/util/timer.hpp>
 using namespace graphlab;
 int numticks = 0;
@@ -8,18 +8,17 @@ void threadfn() {
   timer ti; ti.start();
   while(1) {
     if (ti.current_time() >= 1) break;
-    fiber_group::yield();
+    fiber_control::yield();
     __sync_fetch_and_add(&numticks, 1);
   }
 }
 
 int main(int argc, char** argv) {
-  fiber_group fibers(4, 8192);
   timer ti; ti.start();
   for (int i = 0;i < 100000; ++i) {
-    fibers.launch(threadfn);
+    fiber_control::get_instance().launch(threadfn);
   }
-  fibers.join();
+  fiber_control::get_instance().join();
   std::cout << "Completion in " << ti.current_time() << "s\n";
   std::cout << "Context Switches: " << numticks << "\n";
 }
--- tests/fibo_fiber_test.cpp
+++ tests/fibo_fiber_test.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <boost/bind.hpp>
-#include <graphlab/parallel/fiber.hpp>
+#include <graphlab/parallel/fiber_control.hpp>
 using namespace graphlab;
-fiber_group* fibers;
 
 struct fibonacci_compute_promise {
   mutex* lock;
@@ -23,20 +22,20 @@ void fibonacci(fibonacci_compute_promise* promise) {
     left.lock = &lock;
     left.argument = promise->argument - 1;
     left.result_set = false;
-    left.parent_tid = fiber_group::get_tid();
+    left.parent_tid = fiber_control::get_tid();
 
     right.lock = &lock;
     right.argument = promise->argument - 2;
     right.result_set = false;
-    right.parent_tid = fiber_group::get_tid();
+    right.parent_tid = fiber_control::get_tid();
 
-    fibers->launch(boost::bind(fibonacci, &left));
-    fibers->launch(boost::bind(fibonacci, &right));
+    fiber_control::get_instance().launch(boost::bind(fibonacci, &left));
+    fiber_control::get_instance().launch(boost::bind(fibonacci, &right));
 
     // wait on the left and right promise
     lock.lock();
     while (left.result_set == false || right.result_set == false) {
-      fiber_group::deschedule_self(&lock.m_mut);
+      fiber_control::deschedule_self(&lock.m_mut);
       lock.lock();
     }
     lock.unlock();
@@ -47,13 +46,12 @@ void fibonacci(fibonacci_compute_promise* promise) {
   }
   promise->lock->lock();
   promise->result_set = true;
-  if (promise->parent_tid) fiber_group::schedule_tid(promise->parent_tid);
+  if (promise->parent_tid) fiber_control::schedule_tid(promise->parent_tid);
   promise->lock->unlock();
 }
 
 
 int main(int argc, char** argv) {
-  fibers = new fiber_group(4, 8192);
 
   timer ti; ti.start();
 
@@ -63,13 +61,11 @@ int main(int argc, char** argv) {
   promise.result_set = false;
   promise.argument = 24;
   promise.parent_tid = 0;
-  fibers->launch(boost::bind(fibonacci, &promise));
-  fibers->join();
+  fiber_control::get_instance().launch(boost::bind(fibonacci, &promise));
+  fiber_control::get_instance().join();
   assert(promise.result_set);
   std::cout << "Fib(" << promise.argument << ") = " << promise.result << "\n";
 
   std::cout << "Completion in " << ti.current_time() << "s\n";
-  std::cout << fibers->total_threads_created() << " threads created\n";
-
-  delete fibers;
+  std::cout << fiber_control::get_instance().total_threads_created() << " threads created\n";
 }
