9d77cd85dad09e0103319ee2242870d02d5c4b50
Joey <joseph.e.gonzalez@gmail.com>
2013-08-27 15:45:37 -0700

    Changing code to script to download from github

--- scripts/install_graphlab.sh
+++ scripts/install_graphlab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-hg clone https://code.google.com/p/graphlabapi/ 
-cd graphlabapi
+git clone https://github.com/graphlab-code/graphlab.git
+cd graphlab
 ./configure | tee install_configure_log.txt
 cd release
-make -j2 | tee ../v2_debug_log.txt
+make -j2 | tee ../v2_build_log.txt
 
