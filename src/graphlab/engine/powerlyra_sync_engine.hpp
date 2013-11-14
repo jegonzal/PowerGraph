c3e88dfb051e396002eda935da225a25bbb12159
Rong.Chen <rongchen@sjtu.edu.cn>
2013-11-14 11:19:54 +0800

    modify license note only add new apache lisence with ipads.sjtu to new add files
    is it right ...

--- src/graphlab/engine/powerlyra_sync_engine.hpp
+++ src/graphlab/engine/powerlyra_sync_engine.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009 Carnegie Mellon University.
+ * Copyright (c) 2013 Institute of Parallel and Distributed Systems, Shanghai Jiao Tong University.
  *     All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,10 +16,9 @@
  *
  * For more about this software visit:
  *
- *      http://www.graphlab.ml.cmu.edu
+ *      http://ipads.se.sjtu.edu.cn/projects/powerlyra.html
  *
  */
-
 
 
 #ifndef GRAPHLAB_POWERLYRA_SYNC_ENGINE_HPP
--- src/graphlab/graph/ingress/distributed_hybrid_ginger_ingress.hpp
+++ src/graphlab/graph/ingress/distributed_hybrid_ginger_ingress.hpp
@@ -1,5 +1,5 @@
-/**  
- * Copyright (c) 2009 Carnegie Mellon University. 
+/**
+ * Copyright (c) 2013 Institute of Parallel and Distributed Systems, Shanghai Jiao Tong University.
  *     All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,15 +9,17 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an "AS *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  software distributed under the License is distributed on an "AS
+ *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  *  express or implied.  See the License for the specific language
  *  governing permissions and limitations under the License.
  *
  * For more about this software visit:
  *
- *      http://www.graphlab.ml.cmu.edu
+ *      http://ipads.se.sjtu.edu.cn/projects/powerlyra.html
  *
  */
+
 
 #ifndef GRAPHLAB_DISTRIBUTED_HYBRID_GINGER_INGRESS_HPP
 #define GRAPHLAB_DISTRIBUTED_HYBRID_GINGER_INGRESS_HPP
