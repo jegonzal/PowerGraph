0a7f0a804a2d63c2711b65a563e0e6453b3f56d8
Jesus Cerquides <cerquide@iiia.csic.es>
2014-04-22 13:00:00 +0200

    Some fixes so that distributed_graph_test can run. Identified and reported a bug in boost::multiprecision. Provided a fix so that graphlab is not affected.

--- src/graphlab/util/multiprecision_vertex_id_types.hpp
+++ src/graphlab/util/multiprecision_vertex_id_types.hpp
@@ -1,108 +1,67 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #include <graphlab/serialization/serialization_includes.hpp>
 
+#define MULTIPRECISION_VERTEX_ID_SAVE std::stringstream ss;\
+  ss << x;\
+  std::string s;\
+  ss >> s;\
+  oarc << s;
+
+#define MULTIPRECISION_VERTEX_ID_LOAD std::string s;\
+  iarc >> s;\
+  x = 0;\
+  x.assign(s);
+
 // Integer of 128 bits.
 
 BEGIN_OUT_OF_PLACE_SAVE(oarc, boost::multiprecision::int128_t , x)
-  std::stringstream ss;
-  ss << "0x" << std::hex << x;
-  std::string s;
-  ss >> s;
-  oarc << s;
+  MULTIPRECISION_VERTEX_ID_SAVE
 END_OUT_OF_PLACE_SAVE()
 
 BEGIN_OUT_OF_PLACE_LOAD(iarc, boost::multiprecision::int128_t , x)
-  std::string s;
-  iarc >> s;
-  x.assign(s);
+  MULTIPRECISION_VERTEX_ID_LOAD
 END_OUT_OF_PLACE_LOAD()
-
-namespace boost {
-  namespace multiprecision {
-    inline size_t hash_value (const boost::multiprecision::int128_t &x)
-    {
-      // not a very good hash function, but I just want to get it working first!
-      return static_cast<size_t>(x);
-    }
-  }
-}
 
 // Integer of 256 bits.
 
 BEGIN_OUT_OF_PLACE_SAVE(oarc, boost::multiprecision::int256_t , x)
-  std::stringstream ss;
-  ss << "0x" << std::hex << x;
-  std::string s;
-  ss >> s;
-  oarc << s;
+  MULTIPRECISION_VERTEX_ID_SAVE
 END_OUT_OF_PLACE_SAVE()
 
 BEGIN_OUT_OF_PLACE_LOAD(iarc, boost::multiprecision::int256_t , x)
-  std::string s;
-  iarc >> s;
-  x.assign(s);
+  MULTIPRECISION_VERTEX_ID_LOAD
 END_OUT_OF_PLACE_LOAD()
-
-namespace boost {
-  namespace multiprecision {
-    inline size_t hash_value (const boost::multiprecision::int256_t &x)
-    {
-      // not a very good hash function, but I just want to get it working first!
-      return static_cast<size_t>(x);
-    }
-  }
-}
 
 // Integer of 512 bits.
 
 BEGIN_OUT_OF_PLACE_SAVE(oarc, boost::multiprecision::int512_t , x)
-  std::stringstream ss;
-  ss << "0x" << std::hex << x;
-  std::string s;
-  ss >> s;
-  oarc << s;
+  MULTIPRECISION_VERTEX_ID_SAVE
 END_OUT_OF_PLACE_SAVE()
 
 BEGIN_OUT_OF_PLACE_LOAD(iarc, boost::multiprecision::int512_t , x)
-  std::string s;
-  iarc >> s;
-  x.assign(s);
+  MULTIPRECISION_VERTEX_ID_LOAD
 END_OUT_OF_PLACE_LOAD()
-
-namespace boost {
-  namespace multiprecision {
-    inline size_t hash_value (const boost::multiprecision::int512_t &x)
-    {
-      // not a very good hash function, but I just want to get it working first!
-      return static_cast<size_t>(x);
-    }
-  }
-}
 
 // Integer of 1024 bits.
 
 BEGIN_OUT_OF_PLACE_SAVE(oarc, boost::multiprecision::int1024_t , x)
-  std::stringstream ss;
-  ss << "0x" << std::hex << x;
-  std::string s;
-  ss >> s;
-  oarc << s;
+  MULTIPRECISION_VERTEX_ID_SAVE
 END_OUT_OF_PLACE_SAVE()
 
 BEGIN_OUT_OF_PLACE_LOAD(iarc, boost::multiprecision::int1024_t , x)
-  std::string s;
-  iarc >> s;
-  x.assign(s);
+  MULTIPRECISION_VERTEX_ID_LOAD
 END_OUT_OF_PLACE_LOAD()
 
 namespace boost {
   namespace multiprecision {
-    inline size_t hash_value (const boost::multiprecision::int1024_t &x)
+    template <typename T>
+    inline size_t hash_value (const T &x)
     {
       // not a very good hash function, but I just want to get it working first!
-      return static_cast<size_t>(x);
+      return static_cast<int>(x);
     }
   }
 }
+
 
 
