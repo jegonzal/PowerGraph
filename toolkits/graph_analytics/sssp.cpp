ccbd1516b6f4567b55b12f7ea4aaf7e80add520d
Ubuntu <ubuntu@ip-172-31-27-9.us-west-2.compute.internal>
2014-09-03 05:56:36 +0000

    fixed boost compilation issue with sssp

--- toolkits/graph_analytics/sssp.cpp
+++ toolkits/graph_analytics/sssp.cpp
@@ -25,18 +25,7 @@
 #include <fstream>
 
 
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_stl.hpp>
-#include <boost/unordered_set.hpp>
-
 #include <graphlab.hpp>
-
-#include <graphlab/util/stl_util.hpp>
-
-
-#include <graphlab/macros_def.hpp>
 
 
 /**
@@ -224,7 +213,7 @@ int main(int argc, char** argv) {
   std::string format = "adj";
   std::string exec_type = "synchronous";
   size_t powerlaw = 0;
-  std::vector<graphlab::vertex_id_type> sources;
+  std::vector<unsigned int> sources;
   bool max_degree_source = false;
   clopts.attach_option("graph", graph_dir,
                        "The graph file.  If none is provided "
